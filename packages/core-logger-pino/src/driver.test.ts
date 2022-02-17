cecadf2545255cdf07c83c93462edbf75fc6933f
Brian Faust <hello@basecode.sh>
2022-02-17 06:19:14 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/core-logger-pino/src/driver.test.ts
+++ packages/core-logger-pino/src/driver.test.ts
@@ -14,218 +14,219 @@ let message: string;
 
 let app: Application;
 
-describe("Logger", ({ assert, afterAll, afterEach, beforeAll, beforeEach, it}) => {
-    beforeAll(() => {
-        capcon.startCapture(process.stdout, (stdout) => (message = stdout.toString()));
+describe("Logger", ({ assert, afterAll, afterEach, beforeAll, beforeEach, it }) => {
+	beforeAll(() => {
+		capcon.startCapture(process.stdout, (stdout) => (message = stdout.toString()));
 
-        capcon.startCapture(process.stderr, (stderr) => (message = stderr.toString()));
+		capcon.startCapture(process.stderr, (stderr) => (message = stderr.toString()));
 
-        // @ts-ignore
-        capcon.startCapture(console._stdout, (stdout) => (message = stdout.toString()));
+		// @ts-ignore
+		capcon.startCapture(console._stdout, (stdout) => (message = stdout.toString()));
 
-        // @ts-ignore
-        capcon.startCapture(console._stderr, (stderr) => (message = stderr.toString()));
-    });
+		// @ts-ignore
+		capcon.startCapture(console._stderr, (stderr) => (message = stderr.toString()));
+	});
 
-    afterAll(() => setGracefulCleanup());
+	afterAll(() => setGracefulCleanup());
 
-    beforeEach(async () => {
-        app = new Application(new Container.Container());
-        app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
-        app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("ark-unitnet");
-        app.bind("path.log").toConstantValue(dirSync().name);
+	beforeEach(async () => {
+		app = new Application(new Container.Container());
+		app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
+		app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("ark-unitnet");
+		app.bind("path.log").toConstantValue(dirSync().name);
 
-        logger = await app.resolve<Logger>(PinoLogger).make({
-            levels: {
-                console: process.env.CORE_LOG_LEVEL || "debug",
-                file: process.env.CORE_LOG_LEVEL_FILE || "debug",
-            },
-            fileRotator: {
-                interval: "1d",
-            },
-        });
-    });
+		logger = await app.resolve<Logger>(PinoLogger).make({
+			levels: {
+				console: process.env.CORE_LOG_LEVEL || "debug",
+				file: process.env.CORE_LOG_LEVEL_FILE || "debug",
+			},
+			fileRotator: {
+				interval: "1d",
+			},
+		});
+	});
 
-    afterEach(() => (message = undefined));
+	afterEach(() => (message = undefined));
 
-    it("should not be logged if empty", () => {
-        logger.info(undefined);
+	it("should not be logged if empty", () => {
+		logger.info(undefined);
 
-        assert.undefined(message);
-    });
+		assert.undefined(message);
+	});
 
 	it("should modify the message if it is not a string", () => {
 		logger.info(["Hello World"]);
 
-        assert.match(message.trim(), "[ 'Hello World' ]");
-    });
+		assert.match(message.trim(), "[ 'Hello World' ]");
+	});
 
 	it("should log a message with the [emergency] level", () => {
 		logger.emergency("emergency_message");
 
-
-        assert.match(message, /emergency/);
-        assert.match(message, /emergency_message/);
-    });
+		assert.match(message, /emergency/);
+		assert.match(message, /emergency_message/);
+	});
 
 	it("should log a message with the [alert] level", () => {
 		logger.alert("alert_message");
 
-        assert.match(message, /alert/);
-        assert.match(message, /alert_message/);
-    });
+		assert.match(message, /alert/);
+		assert.match(message, /alert_message/);
+	});
 
 	it("should log a message with the [critical] level", () => {
 		logger.critical("critical_message");
 
-        assert.match(message, /critical/);
-        assert.match(message, /critical_message/);
-    });
+		assert.match(message, /critical/);
+		assert.match(message, /critical_message/);
+	});
 
 	it("should log a message with the [error] level", () => {
 		logger.error("error_message");
 
-        assert.match(message, /error/);
-        assert.match(message, /error_message/);
-    });
+		assert.match(message, /error/);
+		assert.match(message, /error_message/);
+	});
 
 	it("should log a message with the [warning] level", () => {
 		logger.warning("warning_message");
 
-        assert.match(message, /warning/);
-        assert.match(message, /warning_message/);
-    });
+		assert.match(message, /warning/);
+		assert.match(message, /warning_message/);
+	});
 
 	it("should log a message with the [notice] level", () => {
 		logger.notice("notice_message");
 
-        assert.match(message, /notice/);
-        assert.match(message, /notice_message/);
-    });
+		assert.match(message, /notice/);
+		assert.match(message, /notice_message/);
+	});
 
-    it("should log a message with the [info] level", () => {
-        logger.info("info_message");
+	it("should log a message with the [info] level", () => {
+		logger.info("info_message");
 
-        assert.match(message, /info/);
-        assert.match(message, /info_message/);
-    });
+		assert.match(message, /info/);
+		assert.match(message, /info_message/);
+	});
 
-    it("should log a message with the [debug] level", () => {
-        logger.debug("debug_message");
+	it("should log a message with the [debug] level", () => {
+		logger.debug("debug_message");
 
-        assert.match(message, /debug/);
-        assert.match(message, /debug_message/);
-    });
+		assert.match(message, /debug/);
+		assert.match(message, /debug_message/);
+	});
 
-    it("should suppress console output", () => {
-        logger.suppressConsoleOutput(true);
+	it("should suppress console output", () => {
+		logger.suppressConsoleOutput(true);
 
-        logger.info("silent_message");
+		logger.info("silent_message");
 
-        // @TODO Something is working different here
-        // assert.undefined(message);
-        assert.equal(message, "[90m• [39m");
+		// @TODO Something is working different here
+		// assert.undefined(message);
+		assert.equal(message, "[90m• [39m");
 
-        logger.suppressConsoleOutput(false);
+		logger.suppressConsoleOutput(false);
 
-        logger.info("non_silent_message");
-        assert.match(message, /non_silent_message/);
-    });
+		logger.info("non_silent_message");
+		assert.match(message, /non_silent_message/);
+	});
 
-    it("should log error if there is an error on file stream", async () => {
-        const logger = app.resolve<Logger>(PinoLogger);
+	it("should log error if there is an error on file stream", async () => {
+		const logger = app.resolve<Logger>(PinoLogger);
 
-        const writableMock = new Writable({
-            write(chunk, enc, cb) {
-                throw new Error("Stream error");
-            },
-        });
-        // @ts-ignore
-        logger.getFileStream = () => {
-            return writableMock;
-        };
+		const writableMock = new Writable({
+			write(chunk, enc, cb) {
+				throw new Error("Stream error");
+			},
+		});
+		// @ts-ignore
+		logger.getFileStream = () => {
+			return writableMock;
+		};
 
-        await logger.make({
-            levels: {
-                console: "invalid",
-                file: process.env.CORE_LOG_LEVEL_FILE || "debug",
-            },
-            fileRotator: {
-                interval: "1d",
-            },
-        });
+		await logger.make({
+			levels: {
+				console: "invalid",
+				file: process.env.CORE_LOG_LEVEL_FILE || "debug",
+			},
+			fileRotator: {
+				interval: "1d",
+			},
+		});
 
-        writableMock.destroy(new Error("Test error"));
+		writableMock.destroy(new Error("Test error"));
 
-        await sleep(100);
+		await sleep(100);
 
-        assert.match(message, "File stream closed due to an error: Error: Test error");
+		assert.match(message, "File stream closed due to an error: Error: Test error");
 
-        await assert.resolves(() => logger.dispose());
-    });
+		await assert.resolves(() => logger.dispose());
+	});
 
-    it("should rotate the log 3 times", async () => {
-        const app = new Application(new Container.Container());
-        app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
-        app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("ark-unitnet");
-        app.useLogPath(dirSync().name);
+	it("should rotate the log 3 times", async () => {
+		const app = new Application(new Container.Container());
+		app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
+		app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("ark-unitnet");
+		app.useLogPath(dirSync().name);
 
-        const ms = new Date().getMilliseconds();
-        await sleep(1000 - ms + 400);
+		const ms = new Date().getMilliseconds();
+		await sleep(1000 - ms + 400);
 
-        const logger = await app.resolve(PinoLogger).make({
-            levels: {
-                console: process.env.CORE_LOG_LEVEL || "emergency",
-                file: process.env.CORE_LOG_LEVEL_FILE || "emergency",
-            },
-            fileRotator: {
-                interval: "1s",
-            },
-        });
+		const logger = await app.resolve(PinoLogger).make({
+			levels: {
+				console: process.env.CORE_LOG_LEVEL || "emergency",
+				file: process.env.CORE_LOG_LEVEL_FILE || "emergency",
+			},
+			fileRotator: {
+				interval: "1s",
+			},
+		});
 
-        for (let i = 0; i < 3; i++) {
-            logger.info(`Test ${i + 1}`);
+		for (let i = 0; i < 3; i++) {
+			logger.info(`Test ${i + 1}`);
 
-            await sleep(900);
-        }
+			await sleep(900);
+		}
 
-        const files = readdirSync(app.logPath());
-        assert.length(files.filter((file) => file.endsWith(".log.gz")), 3);
-        assert.length(files, 5);
-    });
+		const files = readdirSync(app.logPath());
+		assert.length(
+			files.filter((file) => file.endsWith(".log.gz")),
+			3,
+		);
+		assert.length(files, 5);
+	});
 
-    describe("make", () => {
-        it("should create a file stream if level is valid", () => {
+	describe("make", () => {
+		it("should create a file stream if level is valid", () => {
+			// @ts-ignore
+			assert.defined(logger.combinedFileStream);
+		});
 
-            // @ts-ignore
-            assert.defined(logger.combinedFileStream);
-        });
+		it("should not create a file stream if level not is valid", async () => {
+			const logger = await app.resolve<PinoLogger>(PinoLogger).make({
+				levels: {
+					console: process.env.CORE_LOG_LEVEL || "debug",
+					file: "invalid",
+				},
+				fileRotator: {
+					interval: "1d",
+				},
+			});
 
-        it("should not create a file stream if level not is valid", async () => {
-            const logger = await app.resolve<PinoLogger>(PinoLogger).make({
-                levels: {
-                    console: process.env.CORE_LOG_LEVEL || "debug",
-                    file: "invalid",
-                },
-                fileRotator: {
-                    interval: "1d",
-                },
-            });
+			// @ts-ignore
+			assert.undefined(logger.combinedFileStream);
+		});
+	});
 
-            // @ts-ignore
-            assert.undefined(logger.combinedFileStream);
-        });
-    });
+	describe("dispose", () => {
+		it("should dispose before make", async () => {
+			const logger = await app.resolve<PinoLogger>(PinoLogger);
 
-    describe("dispose", () => {
-        it("should dispose before make", async () => {
-            const logger = await app.resolve<PinoLogger>(PinoLogger);
+			await assert.resolves(() => logger.dispose());
+		});
 
-            await assert.resolves(() => logger.dispose());
-        });
-
-        it("should dispose after make", async () => {
-            await assert.resolves(() => logger.dispose());
-        });
-    });
+		it("should dispose after make", async () => {
+			await assert.resolves(() => logger.dispose());
+		});
+	});
 });
--- packages/core-logger-pino/src/service-provider.test.ts
+++ packages/core-logger-pino/src/service-provider.test.ts
@@ -5,180 +5,179 @@ import { AnySchema } from "joi";
 import { dirSync } from "tmp";
 import { describe } from "@arkecosystem/core-test";
 
-describe("ServiceProvider", ({ assert, afterEach, beforeEach, it}) => {
-    let app: Application;
+describe("ServiceProvider", ({ assert, afterEach, beforeEach, it }) => {
+	let app: Application;
 
-    let serviceProvider: ServiceProvider;
-    let preserveValue = undefined;
-    let writableDefaults;
+	let serviceProvider: ServiceProvider;
+	let preserveValue = undefined;
+	let writableDefaults;
 
-    beforeEach(() => {
-        app = new Application(new Container.Container());
-        app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
+	beforeEach(() => {
+		app = new Application(new Container.Container());
+		app.bind(Container.Identifiers.ConfigFlags).toConstantValue("core");
 
-        serviceProvider = app.resolve<ServiceProvider>(ServiceProvider);
+		serviceProvider = app.resolve<ServiceProvider>(ServiceProvider);
 
-        preserveValue = process.env.CORE_LOG_LEVEL;
+		preserveValue = process.env.CORE_LOG_LEVEL;
 
-        writableDefaults = {...defaults };
-    });
+		writableDefaults = { ...defaults };
+	});
 
-    afterEach(() => {
-        process.env.CORE_LOG_LEVEL = preserveValue;
-    });
+	afterEach(() => {
+		process.env.CORE_LOG_LEVEL = preserveValue;
+	});
 
-    it("should register", async () => {
-        app.bind<Services.Log.LogManager>(Container.Identifiers.LogManager)
-            .to(Services.Log.LogManager)
-            .inSingletonScope();
+	it("should register", async () => {
+		app.bind<Services.Log.LogManager>(Container.Identifiers.LogManager)
+			.to(Services.Log.LogManager)
+			.inSingletonScope();
 
-        await app.get<Services.Log.LogManager>(Container.Identifiers.LogManager).boot();
+		await app.get<Services.Log.LogManager>(Container.Identifiers.LogManager).boot();
 
-        serviceProvider.setConfig(app.resolve(Providers.PluginConfiguration).merge(writableDefaults));
+		serviceProvider.setConfig(app.resolve(Providers.PluginConfiguration).merge(writableDefaults));
 
-        app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("token-network");
-        app.bind("path.log").toConstantValue(dirSync().name);
+		app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("token-network");
+		app.bind("path.log").toConstantValue(dirSync().name);
 
-        await assert.resolves(() => serviceProvider.register());
-    });
+		await assert.resolves(() => serviceProvider.register());
+	});
 
-    it("should be disposable", async () => {
-        app.bind<Services.Log.LogManager>(Container.Identifiers.LogManager)
-            .to(Services.Log.LogManager)
-            .inSingletonScope();
+	it("should be disposable", async () => {
+		app.bind<Services.Log.LogManager>(Container.Identifiers.LogManager)
+			.to(Services.Log.LogManager)
+			.inSingletonScope();
 
-        await app.get<Services.Log.LogManager>(Container.Identifiers.LogManager).boot();
+		await app.get<Services.Log.LogManager>(Container.Identifiers.LogManager).boot();
 
-        serviceProvider.setConfig(app.resolve(Providers.PluginConfiguration).merge(writableDefaults));
+		serviceProvider.setConfig(app.resolve(Providers.PluginConfiguration).merge(writableDefaults));
 
-        app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("token-network");
-        app.bind("path.log").toConstantValue(dirSync().name);
+		app.bind(Container.Identifiers.ApplicationNamespace).toConstantValue("token-network");
+		app.bind("path.log").toConstantValue(dirSync().name);
 
-        app.bind(Container.Identifiers.LogService).toDynamicValue((context: Container.interfaces.Context) =>
-            context.container.get<Services.Log.LogManager>(Container.Identifiers.LogManager).driver(),
-        );
+		app.bind(Container.Identifiers.LogService).toDynamicValue((context: Container.interfaces.Context) =>
+			context.container.get<Services.Log.LogManager>(Container.Identifiers.LogManager).driver(),
+		);
 
-        await assert.resolves(() => serviceProvider.register());
+		await assert.resolves(() => serviceProvider.register());
 
-        await assert.resolves(() => serviceProvider.dispose());
-    });
+		await assert.resolves(() => serviceProvider.dispose());
+	});
 
-    describe("ServiceProvider.configSchema", () => {
-        let serviceProvider: ServiceProvider;
+	describe("ServiceProvider.configSchema", () => {
+		let serviceProvider: ServiceProvider;
 
-        beforeEach(() => {
-            serviceProvider = app.resolve<ServiceProvider>(ServiceProvider);
+		beforeEach(() => {
+			serviceProvider = app.resolve<ServiceProvider>(ServiceProvider);
 
-            for (const key of Object.keys(process.env)) {
-                if (key.includes("CORE_LOG_LEVEL")) {
-                    delete process.env[key];
-                }
-            }
-        });
+			for (const key of Object.keys(process.env)) {
+				if (key.includes("CORE_LOG_LEVEL")) {
+					delete process.env[key];
+				}
+			}
+		});
 
-        it("should validate schema using defaults", async () => {
-            const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+		it("should validate schema using defaults", async () => {
+			const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            assert.undefined(result.error);
+			assert.undefined(result.error);
 
-            assert.string(result.value.levels.console);
-            assert.string(result.value.levels.file);
+			assert.string(result.value.levels.console);
+			assert.string(result.value.levels.file);
 
-            assert.string(result.value.fileRotator.interval);
-        });
+			assert.string(result.value.fileRotator.interval);
+		});
 
-        it("should allow configuration extension", async () => {
-            writableDefaults.customField = "dummy";
+		it("should allow configuration extension", async () => {
+			writableDefaults.customField = "dummy";
 
-            const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+			const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            assert.undefined(result.error);
-            assert.equal(result.value.customField, "dummy");
-        });
+			assert.undefined(result.error);
+			assert.equal(result.value.customField, "dummy");
+		});
 
-        describe("process.env.CORE_LOG_LEVEL", () => {
-            it("should return value of process.env.CORE_LOG_LEVEL if defined", async () => {
-                process.env.CORE_LOG_LEVEL = "dummy";
+		describe("process.env.CORE_LOG_LEVEL", () => {
+			it("should return value of process.env.CORE_LOG_LEVEL if defined", async () => {
+				process.env.CORE_LOG_LEVEL = "dummy";
 
-                const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                assert.undefined(result.error);
-                assert.equal(result.value.levels.console, "dummy");
-            });
-        });
+				assert.undefined(result.error);
+				assert.equal(result.value.levels.console, "dummy");
+			});
+		});
 
-        describe("process.env.CORE_LOG_LEVEL_FILE", () => {
-            it("should return value of process.env.CORE_LOG_LEVEL_FILE if defined", async () => {
-                process.env.CORE_LOG_LEVEL_FILE = "dummy";
+		describe("process.env.CORE_LOG_LEVEL_FILE", () => {
+			it("should return value of process.env.CORE_LOG_LEVEL_FILE if defined", async () => {
+				process.env.CORE_LOG_LEVEL_FILE = "dummy";
 
-                const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				const result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                assert.undefined(result.error);
-                assert.equal(result.value.levels.file, "dummy");
-            });
-        });
+				assert.undefined(result.error);
+				assert.equal(result.value.levels.file, "dummy");
+			});
+		});
 
-        describe("schema restrictions", () => {
-            it("levels is required && is object", async () => {
-                writableDefaults.levels = false;
-                let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+		describe("schema restrictions", () => {
+			it("levels is required && is object", async () => {
+				writableDefaults.levels = false;
+				let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
+				assert.equal(result.error!.message, '"levels" must be of type object');
 
-                assert.equal(result.error!.message, '"levels" must be of type object');
+				delete writableDefaults.levels;
+				result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                delete writableDefaults.levels;
-                result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"levels" is required');
+			});
 
-                assert.equal(result.error!.message, '"levels" is required');
-            });
+			it("levels.console is required && is string", async () => {
+				writableDefaults.levels.console = false;
+				let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            it("levels.console is required && is string", async () => {
-                writableDefaults.levels.console = false;
-                let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"levels.console" must be a string');
 
-                assert.equal(result.error!.message, '"levels.console" must be a string');
+				delete writableDefaults.levels.console;
+				result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                delete writableDefaults.levels.console;
-                result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"levels.console" is required');
+			});
 
-                assert.equal(result.error!.message, '"levels.console" is required');
-            });
+			it("levels.file is required && is string", async () => {
+				writableDefaults.levels.file = false;
+				let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            it("levels.file is required && is string", async () => {
-                writableDefaults.levels.file = false;
-                let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"levels.file" must be a string');
 
-                assert.equal(result.error!.message, '"levels.file" must be a string');
+				delete writableDefaults.levels.file;
+				result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                delete writableDefaults.levels.file;
-                result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"levels.file" is required');
+			});
 
-                assert.equal(result.error!.message, '"levels.file" is required');
-            });
+			it("fileRotator is required && is object", async () => {
+				writableDefaults.fileRotator = false;
+				let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            it("fileRotator is required && is object", async () => {
-                writableDefaults.fileRotator = false;
-                let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"fileRotator" must be of type object');
 
-                assert.equal(result.error!.message, '"fileRotator" must be of type object');
+				delete writableDefaults.fileRotator;
+				result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                delete writableDefaults.fileRotator;
-                result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"fileRotator" is required');
+			});
 
-                assert.equal(result.error!.message, '"fileRotator" is required');
-            });
+			it("fileRotator.interval is required && is string", async () => {
+				writableDefaults.fileRotator.interval = false;
+				let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-            it("fileRotator.interval is required && is string", async () => {
-                writableDefaults.fileRotator.interval = false;
-                let result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
+				assert.equal(result.error!.message, '"fileRotator.interval" must be a string');
 
-                assert.equal(result.error!.message, '"fileRotator.interval" must be a string');
+				delete writableDefaults.fileRotator.interval;
+				result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
 
-                delete writableDefaults.fileRotator.interval;
-                result = (serviceProvider.configSchema() as AnySchema).validate(writableDefaults);
-
-                assert.equal(result.error!.message, '"fileRotator.interval" is required');
-            });
-        });
-    });
+				assert.equal(result.error!.message, '"fileRotator.interval" is required');
+			});
+		});
+	});
 });
