d7942987aa32ae65cefcfef16f313b1981ae8c20
Brian Faust <hello@basecode.sh>
2022-02-17 04:18:38 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/crypto-signatures-schnorr/source/index.ts
+++ packages/crypto-signatures-schnorr/source/index.ts
@@ -1,11 +1,12 @@
+import { Signatory as Contract } from "@arkecosystem/crypto-contracts";
 import { schnorr } from "bcrypto";
 
-export class Hash {
-    public static sign(hash: Buffer, privateKey: Buffer): string {
+export class Signatory implements Contract {
+    public sign(hash: Buffer, privateKey: Buffer): string {
         return schnorr.sign(hash, privateKey).toString("hex");
     }
 
-    public static verify(hash: Buffer, signature: Buffer | string, publicKey: Buffer | string): boolean {
+    public verify(hash: Buffer, signature: Buffer | string, publicKey: Buffer | string): boolean {
         return schnorr.verify(
             hash,
             signature instanceof Buffer ? signature : Buffer.from(signature, "hex"),
