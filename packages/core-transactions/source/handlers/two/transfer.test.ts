2c1de96fae6f77f8f3aef4430ea94ed9910d09e3
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2022-02-18 10:18:22 -0300

    no nested describes

--- packages/core-transactions/source/handlers/two/transfer.test.ts
+++ packages/core-transactions/source/handlers/two/transfer.test.ts
@@ -5,7 +5,6 @@ import { Crypto, Enums, Interfaces, Managers, Transactions, Utils } from "@arkec
 
 import { buildMultiSignatureWallet, buildRecipientWallet, buildSenderWallet, initApp } from "../../../test/app";
 import { TransactionHandlerRegistry } from "../handler-registry";
-import { TransferTransactionHandler } from "../one";
 import { TransactionHandler } from "../transaction";
 import { ColdWalletError, InsufficientBalanceError, SenderWalletMismatchError } from "../../errors";
 
@@ -80,161 +79,149 @@ describe("TransferTransaction", ({ assert, afterEach, beforeEach, it, stub }) =>
 		context.store.restore();
 	});
 
-	describe("bootstrap", () => {
-		it("should resolve", async (context) => {
-			Mocks.TransactionRepository.setTransactions([Mapper.mapTransactionToModel(context.transferTransaction)]);
-			await assert.resolves(() => context.handler.bootstrap());
-		});
+	it("bootstrap should resolve", async (context) => {
+		Mocks.TransactionRepository.setTransactions([Mapper.mapTransactionToModel(context.transferTransaction)]);
+		await assert.resolves(() => context.handler.bootstrap());
 	});
 
-	describe("hasVendorField", () => {
-		it("should return true", (context) => {
-			assert.true((<TransferTransactionHandler>context.handler).hasVendorField());
-		});
+	it("hasVendorField should return true", (context) => {
+		assert.true(context.handler.hasVendorField());
 	});
 
-	describe("throwIfCannotBeApplied", (context) => {
-		it("should not throw", async (context) => {
-			await assert.resolves(() =>
-				context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
-			);
-		});
-
-		it("should not throw - multi sign", async (context) => {
-			await assert.resolves(() =>
-				context.handler.throwIfCannotBeApplied(
-					context.multiSignatureTransferTransaction,
-					context.multiSignatureWallet,
-				),
-			);
-		});
-
-		it("should throw", async (context) => {
-			context.transferTransaction.data.senderPublicKey = "a".repeat(66);
-
-			await assert.rejects(
-				() => context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
-				SenderWalletMismatchError,
-			);
-		});
-
-		it("should throw if wallet has insufficient funds for vote", async (context) => {
-			context.senderWallet.setBalance(Utils.BigNumber.ZERO);
-
-			await assert.rejects(
-				() => context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
-				InsufficientBalanceError,
-			);
-		});
-
-		it("should throw if sender is cold wallet", async (context) => {
-			const coldWallet: Wallets.Wallet = context.factoryBuilder
-				.get("Wallet")
-				.withOptions({
-					nonce: 0,
-					passphrase: passphrases[3],
-				})
-				.make();
-
-			coldWallet.setBalance(Utils.BigNumber.ZERO);
-
-			context.transferTransaction = Transactions.BuilderFactory.transfer()
-				.amount("10000000")
-				.recipientId(context.recipientWallet.getAddress())
-				.nonce("1")
-				.sign(passphrases[3])
-				.build();
-
-			await assert.rejects(
-				() => context.handler.throwIfCannotBeApplied(context.transferTransaction, coldWallet),
-				ColdWalletError,
-			);
-		});
-
-		it("should not throw if recipient is cold wallet", async (context) => {
-			const coldWallet: Wallets.Wallet = context.factoryBuilder
-				.get("Wallet")
-				.withOptions({
-					nonce: 0,
-					passphrase: passphrases[3],
-				})
-				.make();
-
-			coldWallet.setBalance(Utils.BigNumber.ZERO);
-
-			context.transferTransaction = Transactions.BuilderFactory.transfer()
-				.amount("10000000")
-				.recipientId(coldWallet.getAddress())
-				.nonce("1")
-				.sign(passphrases[0])
-				.build();
-
-			await assert.resolves(() =>
-				context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
-			);
-		});
+	it("throwIfCannotBeApplied should not throw", async (context) => {
+		await assert.resolves(() =>
+			context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
+		);
 	});
 
-	describe("throwIfCannotEnterPool", () => {
-		it("should not throw", async (context) => {
-			await assert.resolves(() => context.handler.throwIfCannotEnterPool(context.transferTransaction));
-		});
-
-		it.only("should throw if no wallet is not recipient on the active network", async (context) => {
-			Managers.configManager.set("network.pubKeyHash", 99);
-
-			await assert.rejects(
-				() => context.handler.throwIfCannotEnterPool(context.transferTransaction),
-				Contracts.TransactionPool.PoolError,
-			);
-		});
+	it("throwIfCannotBeApplied should not throw - multi sign", async (context) => {
+		await assert.resolves(() =>
+			context.handler.throwIfCannotBeApplied(
+				context.multiSignatureTransferTransaction,
+				context.multiSignatureWallet,
+			),
+		);
 	});
 
-	describe("apply", () => {
-		it("should be ok", async (context) => {
-			const senderBalance = context.senderWallet.getBalance();
-			const recipientBalance = context.recipientWallet.getBalance();
+	it("throwIfCannotBeApplied should throw", async (context) => {
+		context.transferTransaction.data.senderPublicKey = "a".repeat(66);
 
-			await context.handler.apply(context.transferTransaction);
-
-			assert.equal(
-				context.senderWallet.getBalance(),
-				Utils.BigNumber.make(senderBalance)
-					.minus(context.transferTransaction.data.amount)
-					.minus(context.transferTransaction.data.fee),
-			);
-
-			assert.equal(
-				context.recipientWallet.getBalance(),
-				Utils.BigNumber.make(recipientBalance).plus(context.transferTransaction.data.amount),
-			);
-		});
+		await assert.rejects(
+			() => context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
+			SenderWalletMismatchError,
+		);
 	});
 
-	describe("revert", () => {
-		it("should be ok", async (context) => {
-			const senderBalance = context.senderWallet.getBalance();
-			const recipientBalance = context.recipientWallet.getBalance();
+	it("throwIfCannotBeApplied should throw if wallet has insufficient funds for vote", async (context) => {
+		context.senderWallet.setBalance(Utils.BigNumber.ZERO);
 
-			await context.handler.apply(context.transferTransaction);
+		await assert.rejects(
+			() => context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
+			InsufficientBalanceError,
+		);
+	});
 
-			assert.equal(
-				context.senderWallet.getBalance(),
-				Utils.BigNumber.make(senderBalance)
-					.minus(context.transferTransaction.data.amount)
-					.minus(context.transferTransaction.data.fee),
-			);
+	it("throwIfCannotBeApplied should throw if sender is cold wallet", async (context) => {
+		const coldWallet: Wallets.Wallet = context.factoryBuilder
+			.get("Wallet")
+			.withOptions({
+				nonce: 0,
+				passphrase: passphrases[3],
+			})
+			.make();
 
-			assert.equal(
-				context.recipientWallet.getBalance(),
-				Utils.BigNumber.make(recipientBalance).plus(context.transferTransaction.data.amount),
-			);
+		coldWallet.setBalance(Utils.BigNumber.ZERO);
 
-			await context.handler.revert(context.transferTransaction);
+		context.transferTransaction = Transactions.BuilderFactory.transfer()
+			.amount("10000000")
+			.recipientId(context.recipientWallet.getAddress())
+			.nonce("1")
+			.sign(passphrases[3])
+			.build();
 
-			assert.equal(context.senderWallet.getBalance(), Utils.BigNumber.make(senderBalance));
+		await assert.rejects(
+			() => context.handler.throwIfCannotBeApplied(context.transferTransaction, coldWallet),
+			ColdWalletError,
+		);
+	});
 
-			assert.equal(context.recipientWallet.getBalance(), recipientBalance);
-		});
+	it("throwIfCannotBeApplied should not throw if recipient is cold wallet", async (context) => {
+		const coldWallet: Wallets.Wallet = context.factoryBuilder
+			.get("Wallet")
+			.withOptions({
+				nonce: 0,
+				passphrase: passphrases[3],
+			})
+			.make();
+
+		coldWallet.setBalance(Utils.BigNumber.ZERO);
+
+		context.transferTransaction = Transactions.BuilderFactory.transfer()
+			.amount("10000000")
+			.recipientId(coldWallet.getAddress())
+			.nonce("1")
+			.sign(passphrases[0])
+			.build();
+
+		await assert.resolves(() =>
+			context.handler.throwIfCannotBeApplied(context.transferTransaction, context.senderWallet),
+		);
+	});
+
+	it("throwIfCannotEnterPool should not throw", async (context) => {
+		await assert.resolves(() => context.handler.throwIfCannotEnterPool(context.transferTransaction));
+	});
+
+	it("should throw if no wallet is not recipient on the active network", async (context) => {
+		Managers.configManager.set("network.pubKeyHash", 99);
+
+		await assert.rejects(
+			() => context.handler.throwIfCannotEnterPool(context.transferTransaction),
+			Contracts.TransactionPool.PoolError,
+		);
+	});
+
+	it("apply should be ok", async (context) => {
+		const senderBalance = context.senderWallet.getBalance();
+		const recipientBalance = context.recipientWallet.getBalance();
+
+		await context.handler.apply(context.transferTransaction);
+
+		assert.equal(
+			context.senderWallet.getBalance(),
+			Utils.BigNumber.make(senderBalance)
+				.minus(context.transferTransaction.data.amount)
+				.minus(context.transferTransaction.data.fee),
+		);
+
+		assert.equal(
+			context.recipientWallet.getBalance(),
+			Utils.BigNumber.make(recipientBalance).plus(context.transferTransaction.data.amount),
+		);
+	});
+
+	it("revert should be ok", async (context) => {
+		const senderBalance = context.senderWallet.getBalance();
+		const recipientBalance = context.recipientWallet.getBalance();
+
+		await context.handler.apply(context.transferTransaction);
+
+		assert.equal(
+			context.senderWallet.getBalance(),
+			Utils.BigNumber.make(senderBalance)
+				.minus(context.transferTransaction.data.amount)
+				.minus(context.transferTransaction.data.fee),
+		);
+
+		assert.equal(
+			context.recipientWallet.getBalance(),
+			Utils.BigNumber.make(recipientBalance).plus(context.transferTransaction.data.amount),
+		);
+
+		await context.handler.revert(context.transferTransaction);
+
+		assert.equal(context.senderWallet.getBalance(), Utils.BigNumber.make(senderBalance));
+
+		assert.equal(context.recipientWallet.getBalance(), recipientBalance);
 	});
 });
