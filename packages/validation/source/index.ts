53d564ed9f919676be38897bcb167cbf16b95395
Brian Faust <hello@basecode.sh>
2022-02-19 14:27:30 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/validation/source/index.ts
+++ packages/validation/source/index.ts
@@ -1,42 +1,37 @@
 import { ISchemaValidationResult } from "@arkecosystem/crypto-contracts";
 import Ajv from "ajv";
-import ajvKeywords from "ajv-keywords";
-
-import { signedSchema, strictSchema, TransactionSchema } from "../transactions/types/schemas";
-import { formats } from "./formats";
-import { keywords } from "./keywords";
-import { schemas } from "./schemas";
+import keywords from "ajv-keywords";
 
 export class Validator {
 	readonly #ajv: Ajv.Ajv;
-	readonly #transactionSchemas: Map<string, TransactionSchema> = new Map<string, TransactionSchema>();
 
 	public constructor(options: Record<string, any>) {
 		this.#ajv = new Ajv({
 			$data: true,
 			extendRefs: true,
 			removeAdditional: true,
-			schemas,
 			...options,
 		});
 
-		ajvKeywords(this.#ajv);
-
-		for (const addKeyword of keywords) {
-			addKeyword(this.#ajv);
-		}
-
-		for (const addFormat of formats) {
-			addFormat(this.#ajv);
-		}
+		keywords(this.#ajv);
 	}
 
 	public getInstance(): Ajv.Ajv {
 		return this.#ajv;
 	}
 
-	public validate<T = any>(schemaKeyReference: string | boolean | object, data: T): ISchemaValidationResult<T> {
-		return this.validateSchema(this.#ajv, schemaKeyReference, data);
+	public async validate<T = any>(schemaKeyReference: string | boolean | object, data: T): Promise<ISchemaValidationResult<T>> {
+		try {
+			await this.#ajv.validate(schemaKeyReference, data);
+
+			return {
+				error: this.#ajv.errors ? this.#ajv.errorsText() : undefined,
+				errors: this.#ajv.errors || undefined,
+				value: data,
+			};
+		} catch (error) {
+			return { error: error.stack, errors: [], value: undefined };
+		}
 	}
 
 	public addFormat(name: string, format: Ajv.FormatDefinition): void {
@@ -57,60 +52,5 @@ export class Validator {
 
 	public removeSchema(schemaKeyReference: string | boolean | object | RegExp): void {
 		this.#ajv.removeSchema(schemaKeyReference);
-	}
-
-	public extendTransaction(schema: TransactionSchema, remove?: boolean) {
-		this.extendTransactionSchema(this.#ajv, schema, remove);
-	}
-
-	private validateSchema<T = any>(
-		ajv: Ajv.Ajv,
-		schemaKeyReference: string | boolean | object,
-		data: T,
-	): ISchemaValidationResult<T> {
-		try {
-			ajv.validate(schemaKeyReference, data);
-
-			const error = ajv.errors ? ajv.errorsText() : undefined;
-
-			return { error, errors: ajv.errors || undefined, value: data };
-		} catch (error) {
-			return { error: error.stack, errors: [], value: undefined };
-		}
-	}
-
-	private extendTransactionSchema(ajv: Ajv.Ajv, schema: TransactionSchema, remove?: boolean) {
-		if (ajv.getSchema(schema.$id)) {
-			remove = true;
-		}
-
-		if (remove) {
-			this.#transactionSchemas.delete(schema.$id);
-
-			ajv.removeSchema(schema.$id);
-			ajv.removeSchema(`${schema.$id}Signed`);
-			ajv.removeSchema(`${schema.$id}Strict`);
-		}
-
-		this.#transactionSchemas.set(schema.$id, schema);
-
-		ajv.addSchema(schema);
-		ajv.addSchema(signedSchema(schema));
-		ajv.addSchema(strictSchema(schema));
-
-		this.updateTransactionArray(ajv);
-	}
-
-	private updateTransactionArray(ajv: Ajv.Ajv) {
-		ajv.removeSchema("block");
-		ajv.removeSchema("transactions");
-
-		ajv.addSchema({
-			$id: "transactions",
-			additionalItems: false,
-			items: { anyOf: [...this.#transactionSchemas.keys()].map((schema) => ({ $ref: `${schema}Signed` })) },
-			type: "array",
-		});
-		ajv.addSchema(schemas.block);
 	}
 }
