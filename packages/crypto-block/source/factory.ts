f4f958eb2d34e5028dfaf04cf551fa196c822692
Brian Faust <hello@basecode.sh>
2022-02-21 06:07:47 +0200

    Restore factory.ts

--- packages/crypto-block/source/factory.ts
+++ packages/crypto-block/source/factory.ts
@@ -1,59 +1,42 @@
-import { Container } from "@arkecosystem/container";
-import { BINDINGS, BlockFactoryInstance, IBlock, IBlockData, IBlockJson, IHashFactory, IKeyPair, ITransaction, Signatory } from "@arkecosystem/crypto-contracts";
+import { HashFactory } from "@arkecosystem/crypto-hash-bcrypto";
+import { Signatory } from "@arkecosystem/crypto-signature-ecdsa";
+import { IBlock, IBlockData, IBlockJson, IKeyPair, ITransaction } from "@arkecosystem/crypto-contracts";
 import { BigNumber } from "@arkecosystem/utils";
 import { Block } from "./block";
 import { Deserializer } from "./deserializer";
 import { Serializer } from "./serializer";
 import { Configuration } from "@arkecosystem/crypto-config";
-import { applySchema } from "./utils";
-import { IdFactory } from "./id.factory";
 
-@Container.injectable()
 export class BlockFactory {
-	@Container.inject(BINDINGS.Configuration)
-	private readonly configuration: Configuration;
+	readonly #configuration: Configuration;
 
-	@Container.inject(BINDINGS.Block.Serializer)
-	private readonly serializer: Serializer; // @TODO: create contract for block serializer
-
-	@Container.inject(BINDINGS.Block.Deserializer)
-	private readonly deserializer: Deserializer; // @TODO: create contract for block deserializer
-
-	@Container.inject(BINDINGS.Block.FactoryInstance)
-	private readonly blockFactory: BlockFactoryInstance; // @TODO: create contract for block deserializer
-
-	@Container.inject(BINDINGS.Block.IdFactory)
-	private readonly idFactory: IdFactory;
-
-	@Container.inject(BINDINGS.HashFactory)
-	private readonly hashFactory: IHashFactory;
-
-	@Container.inject(BINDINGS.SignatureFactory)
-	private readonly signatureFactory: Signatory;
+	public constructor(configuration: Configuration) {
+		this.#configuration = configuration;
+	}
 
 	// @todo: add a proper type hint for data
 	public async make(data: any, keys: IKeyPair): Promise<IBlock | undefined> {
 		data.generatorPublicKey = keys.publicKey;
 
-		const payloadHash: Buffer = this.serializer.serialize(data, false);
-		const hash: Buffer = await this.hashFactory.sha256(payloadHash);
+		const payloadHash: Buffer = new Serializer(this.#configuration).serialize(data, false);
+		const hash: Buffer = await new HashFactory().sha256(payloadHash);
 
-		data.blockSignature = await this.signatureFactory.sign(hash, Buffer.from(keys.privateKey, "hex"));
-
-		data.id = await this.idFactory.make(data);
+		data.blockSignature = await new Signatory().sign(hash, Buffer.from(keys.privateKey, "hex"));
+		// @ts-ignore
+		data.id = new Block(this.#configuration, {}).getId(data);
 
 		return this.fromData(data);
 	}
 
-	public async fromHex(hex: string): Promise<IBlock> {
+	public fromHex(hex: string): IBlock {
 		return this.fromSerialized(Buffer.from(hex, "hex"));
 	}
 
-	public async fromBytes(buff: Buffer): Promise<IBlock> {
+	public fromBytes(buff: Buffer): IBlock {
 		return this.fromSerialized(buff);
 	}
 
-	public async fromJson(json: IBlockJson): Promise<IBlock | undefined> {
+	public fromJson(json: IBlockJson): IBlock | undefined {
 		// @ts-ignore
 		const data: IBlockData = { ...json };
 		data.totalAmount = BigNumber.make(data.totalAmount);
@@ -70,15 +53,17 @@ export class BlockFactory {
 		return this.fromData(data);
 	}
 
-	public async fromData(
+	public fromData(
 		data: IBlockData,
 		options: { deserializeTransactionsUnchecked?: boolean } = {},
-	): Promise<IBlock | undefined> {
+	): IBlock | undefined {
+		// @ts-ignore
+		const block: IBlockData | undefined = new Block(this.#configuration, {}).applySchema(data);
 
-		if (await applySchema(data)) {
-			const serialized: Buffer = this.serializer.serializeWithTransactions(data);
-			const block: IBlock = this.blockFactory({
-				...(await this.deserializer.deserialize(serialized, false, options)),
+		if (block) {
+			const serialized: Buffer = new Serializer(this.#configuration).serializeWithTransactions(data);
+			const block: IBlock = new Block(this.#configuration, {
+				...new Deserializer(this.#configuration).deserialize(serialized, false, options),
 				id: data.id,
 			});
 			block.serialized = serialized.toString("hex");
@@ -89,16 +74,19 @@ export class BlockFactory {
 		return undefined;
 	}
 
-	private async fromSerialized(serialized: Buffer): Promise<IBlock> {
-		const deserialized: { data: IBlockData; transactions: ITransaction[] } = await this.deserializer.deserialize(serialized);
+	private fromSerialized(serialized: Buffer): IBlock {
+		const deserialized: { data: IBlockData; transactions: ITransaction[] } = new Deserializer(
+			this.#configuration,
+		).deserialize(serialized);
 
-		const validated: IBlockData | undefined = await applySchema(deserialized.data);
+		// @ts-ignore
+		const validated: IBlockData | undefined = new Block(this.#configuration, {}).applySchema(deserialized.data);
 
 		if (validated) {
 			deserialized.data = validated;
 		}
 
-		const block: IBlock = new Block(this.configuration, deserialized);
+		const block: IBlock = new Block(this.#configuration, deserialized);
 		block.serialized = serialized.toString("hex");
 
 		return block;
