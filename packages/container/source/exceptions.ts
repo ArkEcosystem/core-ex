e467aed900fcf13aa295642adf1eaa23cc7ccd17
Brian Faust <hello@basecode.sh>
2022-02-21 04:58:11 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ packages/container/source/exceptions.ts
@@ -0,0 +1,39 @@
+export class Exception extends Error {
+	public constructor(message: string, code?: string) {
+		super(message);
+
+		Object.defineProperty(this, "message", {
+			enumerable: false,
+			value: code ? `${code}: ${message}` : message,
+		});
+
+		Object.defineProperty(this, "name", {
+			enumerable: false,
+			value: this.constructor.name,
+		});
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export class ContainerException extends Exception {}
+
+export class EntryNotFound extends ContainerException {}
+
+export class BindingResolution extends ContainerException {}
+
+export class ParameterNotFound extends ContainerException {}
+
+export class ServiceNotFound extends ContainerException {}
+
+export class InvalidBindingName extends ContainerException {
+	public constructor(name: string) {
+		super(`The name [${name}] is reserved.`);
+	}
+}
+
+export class DriverCannotBeResolved extends ContainerException {
+	public constructor(name: string) {
+		super(`Unable to resolve driver for [${name}].'`);
+	}
+}
---
+++ packages/container/source/ioc/index.ts
@@ -0,0 +1,6 @@
+import "reflect-metadata";
+
+export * from "./decorator";
+export * from "./identifiers";
+export * as Selectors from "./selectors";
+export * from "inversify";
