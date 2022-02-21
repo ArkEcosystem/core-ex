bcc4ce912ac8583eb14dab45dc10c666697e7acf
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-21 13:30:10 +0100

    test(core-webhooks): utils.test.ts move data inder describe

--- packages/core-webhooks/source/server/utils.test.ts
+++ packages/core-webhooks/source/server/utils.test.ts
@@ -5,27 +5,27 @@ import { notFound } from "@hapi/boom";
 import { Webhook } from "../interfaces";
 import { respondWithResource } from "./utils";
 
-const data: Webhook = {
-	conditions: [
-		{
-			condition: "eq",
-			key: "generatorPublicKey",
-			value: "test-generator",
-		},
-		{
-			condition: "gte",
-			key: "fee",
-			value: "123",
-		},
-	],
-	enabled: true,
-	event: Enums.BlockEvent.Forged,
-	id: "dummy_id",
-	target: "https://httpbin.org/post",
-	token: "ark",
-};
-
 describe("Utils", ({ it, assert }) => {
+	const data: Webhook = {
+		conditions: [
+			{
+				condition: "eq",
+				key: "generatorPublicKey",
+				value: "test-generator",
+			},
+			{
+				condition: "gte",
+				key: "fee",
+				value: "123",
+			},
+		],
+		enabled: true,
+		event: Enums.BlockEvent.Forged,
+		id: "dummy_id",
+		target: "https://httpbin.org/post",
+		token: "ark",
+	};
+
 	it("respondWithResource should return transformed resource", () => {
 		assert.equal(respondWithResource(data), { data: data });
 	});
