0b98c7511f1faf45e03c7c341396fbb0c1289562
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-22 13:42:58 +0100

    test(core-test-framework): abort-stopped-process.test.ts

---
+++ packages/core-cli/source/actions/abort-stopped-process.test.ts
@@ -0,0 +1,36 @@
+import { describe } from "@arkecosystem/core-test-framework";
+
+import { ProcessIdentifier } from "../contracts";
+import { Container, Identifiers } from "../ioc";
+import { ProcessManager } from "../services";
+import { AbortStoppedProcess } from "./abort-stopped-process";
+
+describe<{
+	action: AbortStoppedProcess;
+}>("AbortErroredProcess", ({ beforeEach, it, assert, stub }) => {
+	const processName = "ark-core";
+
+	const processManager: Partial<ProcessManager> = {
+		isStopped: (id: ProcessIdentifier) => false,
+	};
+
+	beforeEach((context) => {
+		const app = new Container();
+		app.bind(Identifiers.ProcessManager).toConstantValue(processManager);
+		context.action = app.resolve(AbortStoppedProcess);
+	});
+
+	it("should not throw if the process is running", ({ action }) => {
+		const spyIsErrored = stub(processManager, "isStopped").returnValue(false);
+
+		action.execute(processName);
+		spyIsErrored.calledOnce();
+	});
+
+	it("should throw if the process is not running", ({ action }) => {
+		const spyIsErrored = stub(processManager, "isStopped").returnValue(true);
+
+		assert.throws(() => {action.execute(processName)},`The "${processName}" process is not running.`);
+		spyIsErrored.calledOnce();
+	});
+});
