4f934e5b687e6df790283ff08534b43b2b8ea95b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-23 15:08:10 +0100

    test(core-cli): discover-commands.test.ts

--- packages/core-cli/source/commands/discover-commands.test.ts
+++ packages/core-cli/source/commands/discover-commands.test.ts
@@ -23,32 +23,20 @@ describe<{ DiscoverCommands; cmd: DiscoverCommands }>("DiscoverCommands", ({ bef
 		assert.object(commands);
 		assert.gt(Object.keys(commands).length, 0);
 	});
-	// describe("#within", () => {
-	// 	it("should discover commands within the given directory", () => {
-	// 		const commandPath: string = resolve(__dirname, "../../../core/distribution/commands");
 
-	// 		const commands = cmd.within(commandPath);
+	it("#from - should not discover commands if no packages are passed in", ({cmd}) => {
+		const commands = cmd.from([]);
 
-	// 		expect(commands).toBeObject();
-	// 		expect(commands).not.toBeEmpty();
-	// 	});
-	// });
+		assert.object(commands);
+		assert.equal(Object.keys(commands).length, 0);
+	});
 
-	// describe("#from", () => {
-	// 	it("should not discover commands if no packages are passed in", () => {
-	// 		const commands = cmd.from([]);
-
-	// 		expect(commands).toBeObject();
-	// 		expect(commands).toBeEmpty();
-	// 	});
-
-	// 	it.skip("should discover commands within the given packages", () => {
-	// 		const commandPath: string = resolve(__dirname, "./distribution");
-
-	// 		const commands = cmd.from([commandPath]);
-
-	// 		expect(commands).toBeObject();
-	// 		expect(commands).toContainAllKeys(["help"]);
-	// 	});
+	// it.skip("should discover commands within the given packages", () => {
+	// 	const commandPath: string = resolve(__dirname, "./distribution");
+	//
+	// 	const commands = cmd.from([commandPath]);
+	//
+	// 	expect(commands).toBeObject();
+	// 	expect(commands).toContainAllKeys(["help"]);
 	// });
 });
