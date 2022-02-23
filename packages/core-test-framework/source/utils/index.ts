b57cce20a2d81632ed5616fb3832d781494ac998
Brian Faust <hello@basecode.sh>
2022-02-23 14:10:39 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/core-test-framework/source/utils/index.ts
+++ packages/core-test-framework/source/utils/index.ts
@@ -2,6 +2,5 @@ export * from "./api";
 export * from "./api-http-client";
 export * from "./api-inject-client";
 export * from "./generic";
-export * as Mapper from "./mapper";
 export * from "./rest-client";
 export * from "./transaction-factory";
