12503acc822effcc20cc2f675cac642f3664d4cd
Josip Crnković <jcrnkovic95@gmail.com>
2022-02-23 16:43:41 +0100

    improve validation benchmarks

---
+++ packages/crypto/benchmark/validation.js
@@ -0,0 +1,22 @@
+const { Validator } = require("../distribution/validation");
+
+const iterations = 10000000;
+const validator = Validator.make();
+
+const times = (iterations, callback) => {
+	for (let index = 0; index < iterations; index++) {
+		callback();
+	}
+}
+
+exports["ajv.alphanumeric"] = () => {
+	times(iterations, () => validator.validate("alphanumeric", "123456789"));
+};
+
+exports["ajv.address"] = () => {
+	times(iterations, () => validator.validate("address", [
+		"123456789ABCDEFGHJKLMNPQRSTUVWXYZa",
+		"123456789ABCDEFGHJKLMNPQRSTUVWXYZa",
+		"123456789ABCDEFGHJKLMNPQRSTUVWXYZa",
+	]));
+};
