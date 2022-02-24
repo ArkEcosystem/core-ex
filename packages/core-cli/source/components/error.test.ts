cf327d5a52464fea35c7d611de86ef26fe303b56
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 13:01:46 +0000

    test(core-cli): error.test.ts

---
+++ packages/core-cli/source/components/error.test.ts
@@ -0,0 +1,28 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import { white } from "kleur";
+
+import { Identifiers } from "../ioc";
+import { Error } from "./error";
+
+describe<{
+	component: Error
+	cli: Console
+}>("Error", ({beforeEach, it, assert, spy}) => {
+	beforeEach((context) => {
+		context.cli = new Console();
+		context.cli.app.rebind(Identifiers.Error).to(Error).inSingletonScope();
+		context.component = context.cli.app.get(Identifiers.Error);
+	});
+
+	it("should render the component", async ({component, cli}) => {
+		const spyOnError = spy(cli.app.get(Identifiers.Logger), "error")
+
+		component.render("Hello World");
+
+		spyOnError.calledWith(white().bgRed(`[ERROR] Hello World`));
+	});
+});
+
+
+
+
