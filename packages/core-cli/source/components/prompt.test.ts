2242eb339e1b199acfffd4207d10d3933c520e9a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 14:23:29 +0000

    test(core-cli): prompt.test.ts

---
+++ packages/core-cli/source/components/prompt.test.ts
@@ -0,0 +1,29 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import prompts from "prompts";
+
+import { Identifiers } from "../ioc";
+import { Prompt } from "./prompt";
+
+describe<{
+	component: Prompt
+	cli: Console
+}>("Log", ({beforeEach, it, assert, spy}) => {
+	beforeEach((context) => {
+		context.cli = new Console();
+		context.cli.app.rebind(Identifiers.Prompt).to(Prompt).inSingletonScope();
+		context.component = context.cli.app.get(Identifiers.Prompt);
+	});
+
+	it("should render the component", async ({component, cli}) => {
+		prompts.inject(["johndoe"]);
+
+		assert.equal( await
+			component.render({
+				message: "What's your twitter handle?",
+				name: "value",
+				type: "text",
+			}),
+		{ value: "johndoe" });
+	});
+});
+
