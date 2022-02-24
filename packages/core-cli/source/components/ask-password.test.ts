4cd445b6495c0ea714f6a4d05322e48c894ebbbc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 12:34:53 +0000

    test(core-cli): ask-password.test.ts

---
+++ packages/core-cli/source/components/ask-password.test.ts
@@ -0,0 +1,21 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import prompts from "prompts";
+
+import { Identifiers } from "../ioc";
+import { AskPassword } from "./ask-password";
+
+describe<{
+	component: AskPassword
+}>("AskPassword", ({beforeEach, it, assert}) => {
+	beforeEach((context) => {
+		const cli = new Console();
+		cli.app.rebind(Identifiers.AskPassword).to(AskPassword).inSingletonScope();
+		context.component = cli.app.get(Identifiers.AskPassword);
+	});
+
+	it("should render the component", async ({component}) => {
+		prompts.inject(["password"]);
+
+		assert.equal(await component.render("Hello World"), "password");
+	});
+});
