c1d89758e4dbb3f13ece45b7a1ff253835d211db
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 13:03:26 +0000

    test(core-cli): fatal.test.ts

---
+++ packages/core-cli/source/components/fatal.test.ts
@@ -0,0 +1,28 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import { white } from "kleur";
+
+import { Identifiers } from "../ioc";
+import { Fatal } from "./fatal";
+
+describe<{
+	component: Fatal
+	cli: Console
+}>("Fatal", ({beforeEach, it, assert, spy}) => {
+	beforeEach((context) => {
+		context.cli = new Console();
+		context.cli.app.rebind(Identifiers.Fatal).to(Fatal).inSingletonScope();
+		context.component = context.cli.app.get(Identifiers.Fatal);
+	});
+
+	it("should render the component", async ({component, cli}) => {
+		const spyOnError = spy(cli.app.get(Identifiers.Logger), "error")
+
+		assert.throws(() => component.render("Hello World"),"Hello World");
+
+		spyOnError.calledWith(white().bgRed(`[ERROR] Hello World`));
+	});
+});
+
+
+
+
