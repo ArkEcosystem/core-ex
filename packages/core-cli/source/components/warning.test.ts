4ad218dd336b7ffbc181ab1a76beb844802de20a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 14:51:17 +0000

    test(core-cli): warning.test.ts

---
+++ packages/core-cli/source/components/warning.test.ts
@@ -0,0 +1,25 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import { white } from "kleur";
+
+import { Identifiers } from "../ioc";
+import { Warning } from "./warning";
+
+describe<{
+	component: Warning
+	cli: Console
+}>("Warning", ({beforeEach, it, assert, spy}) => {
+	beforeEach((context) => {
+		context.cli = new Console();
+		context.cli.app.rebind(Identifiers.Warning).to(Warning).inSingletonScope();
+		context.component = context.cli.app.get(Identifiers.Warning);
+	});
+
+	it("should render the component", async ({component, cli}) => {
+		const spyOnLog = spy(cli.app.get(Identifiers.Logger), "warning")
+
+		component.render("Hello World");
+
+		spyOnLog.calledWith(white().bgYellow(`[WARNING] Hello World`));
+	});
+});
+
