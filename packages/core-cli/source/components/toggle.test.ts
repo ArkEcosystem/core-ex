d0d1a7a737cc393c4a8bdc78e39cfb99911dc37b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 14:49:34 +0000

    test(core-cli): toggle.test.ts

---
+++ packages/core-cli/source/components/toggle.test.ts
@@ -0,0 +1,23 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+
+import { Identifiers } from "../ioc";
+import { Toggle } from "./toggle";
+
+describe<{
+	component: Toggle
+	cli: Console
+}>("Log", ({beforeEach, it, assert, spy}) => {
+	beforeEach((context) => {
+		context.cli = new Console();
+		context.cli.app.rebind(Identifiers.Toggle).to(Toggle).inSingletonScope();
+		context.component = context.cli.app.get(Identifiers.Toggle);
+	});
+
+	// TODO: Check later
+	// it("should render the component", async ({component, cli}) => {
+	// 	const spyOnLog = spy(cli.app.get(Identifiers.Logger), "log")
+
+	// 	assert.equal(await component.render("Hello World"), "yes");
+	// });
+});
+
