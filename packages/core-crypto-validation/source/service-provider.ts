593c59a4292f5758bee5ff37b74d5c2a7f904176
Brian Faust <hello@basecode.sh>
2022-02-24 07:04:27 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/core-crypto-validation/source/service-provider.ts
+++ packages/core-crypto-validation/source/service-provider.ts
@@ -1,7 +1,38 @@
 import { Providers } from "@arkecosystem/core-kernel";
+import { BINDINGS, IValidator } from "@packages/core-crypto-contracts/distribution";
+
+import { registerFormats } from "./formats";
+import { registerKeywords } from "./keywords";
+import { schemas } from "./schemas";
 
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
-		//
+		const validator: IValidator = this.app.get(BINDINGS.Validator);
+
+		await this.registerFormats(validator);
+
+		await this.registerKeywords(validator);
+
+		await this.registerSchemas(validator);
+	}
+
+	private async registerFormats(validator: IValidator): Promise<void> {
+		for (const [name, format] of Object.entries(registerFormats(this.app.get(BINDINGS.Configuration)))) {
+			// @ts-ignore
+			validator.addFormat(name, format);
+		}
+	}
+
+	private async registerKeywords(validator: IValidator): Promise<void> {
+		for (const [name, format] of Object.entries(registerKeywords(this.app.get(BINDINGS.Configuration)))) {
+			// @ts-ignore
+			validator.addFormat(name, format);
+		}
+	}
+
+	private async registerSchemas(validator: IValidator): Promise<void> {
+		for (const schema of Object.values(schemas)) {
+			validator.addSchema(schema);
+		}
 	}
 }
