6e8163447cb9fd054bb39c6941e46a1365dbc09a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-24 12:30:45 +0000

    test(core-cli): ask-hidden.test.ts

---
+++ packages/core-cli/source/components/ask-hidden.test.ts
@@ -0,0 +1,22 @@
+import { Console, describe } from "@arkecosystem/core-test-framework";
+import prompts from "prompts";
+
+import { Identifiers } from "../ioc";
+import { AskHidden } from "./ask-hidden";
+
+describe<{
+	component: AskHidden
+}>("AskHidden", ({beforeEach, it, assert}) => {
+	beforeEach((context) => {
+		const cli = new Console();
+		cli.app.rebind(Identifiers.AskHidden).to(AskHidden).inSingletonScope();
+		context.component = cli.app.get(Identifiers.AskHidden);
+	});
+	
+
+	it("should render the component", async ({component}) => {
+		prompts.inject(["hidden"]);
+
+		assert.equal(await component.render("Hello World"),"hidden");
+	});
+});
