af98e7856dc6eeaebdf0992acadc11f0aee45243
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2022-02-25 10:38:42 -0300

    cr updates

--- packages/core-forger/source/errors.test.ts
+++ packages/core-forger/source/errors.test.ts
@@ -2,30 +2,30 @@ import { describe } from "../../core-test-framework/source";
 
 import { ForgerError, HostNoResponseError, RelayCommunicationError } from "./errors";
 
-describe("Errors", ({ assert, it, spy }) => {
-	it("should construct base ForgerError", () => {
+describe("Errors", ({ assert, it}) => {
+	it("should construct base ForgerError", async () => {
 		const message = "I am an error";
 		const error = new ForgerError(message);
-		assert.rejects(() => {
+		await assert.rejects(() => {
 			throw error;
 		}, message);
 		assert.defined(error.stack);
 	});
 
-	it("should construct RelayCommunicationError", () => {
+	it("should construct RelayCommunicationError", async () => {
 		const message = "custom message";
 		const endpoint = "test_endpoint";
 		const error = new RelayCommunicationError(endpoint, message);
-		assert.rejects(() => {
+		await assert.rejects(() => {
 			throw error;
 		}, `Request to ${endpoint} failed, because of '${message}'.`);
 		assert.defined(error.stack);
 	});
 
-	it("should construct HostNoResponseError", () => {
+	it("should construct HostNoResponseError", async () => {
 		const host = "custom host";
 		const error = new HostNoResponseError(host);
-		assert.rejects(() => {
+		await assert.rejects(() => {
 			throw error;
 		}, `${host} didn't respond. Trying again later.`);
 		assert.defined(error.stack);
