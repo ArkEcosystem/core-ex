37ff0d100efd73bb12b1c14e521ffa8461d92d9b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-02-28 17:13:18 +0000

    test(core-cli): fix discover-commands.test.ts

--- packages/core-cli/source/commands/discover-commands.test.ts
+++ packages/core-cli/source/commands/discover-commands.test.ts
@@ -1,7 +1,8 @@
-import { Console, describe } from "../../../core-test-framework";
-import { DiscoverCommands } from "./discover-commands";
 import { resolve } from "path";
 import { setGracefulCleanup } from "tmp";
+
+import { Console, describe } from "../../../core-test-framework";
+import { DiscoverCommands } from "./discover-commands";
 
 describe<{ DiscoverCommands; cmd: DiscoverCommands }>("DiscoverCommands", ({ beforeEach, afterAll, it, assert }) => {
 	beforeEach((context) => {
@@ -15,7 +16,6 @@ describe<{ DiscoverCommands; cmd: DiscoverCommands }>("DiscoverCommands", ({ bef
 	});
 
 	it("#within - should discover commands within the given directory", ({ cmd }) => {
-		// const commandPath: string = resolve(__dirname, "../../../core/distribution/commands");
 		const commandPath: string = resolve("../core/distribution/commands");
 
 		const commands = cmd.within(commandPath);
@@ -31,12 +31,21 @@ describe<{ DiscoverCommands; cmd: DiscoverCommands }>("DiscoverCommands", ({ bef
 		assert.equal(Object.keys(commands).length, 0);
 	});
 
-	// it.skip("should discover commands within the given packages", () => {
-	// 	const commandPath: string = resolve(__dirname, "./distribution");
-	//
-	// 	const commands = cmd.from([commandPath]);
-	//
-	// 	expect(commands).toBeObject();
-	// 	expect(commands).toContainAllKeys(["help"]);
-	// });
+	it("#from - should not discover commands if package path is invalid", ({ cmd }) => {
+		const commands = cmd.from(["dummy/path"]);
+
+		assert.object(commands);
+		assert.equal(Object.keys(commands).length, 0);
+	});
+
+	it("#from - should discover commands within the given packages", ({ cmd }) => {
+		const commandPath: string = resolve(__dirname, "../../test", "./pkg_distribution");
+
+		console.log(commandPath);
+
+		const commands = cmd.from([commandPath]);
+
+		assert.object(commands);
+		assert.true(Object.keys(commands).includes("help"));
+	});
 });
