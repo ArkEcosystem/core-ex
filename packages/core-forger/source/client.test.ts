bb7c3061ab15eff25c2eae3dde256639791b9d30
marianogoldman <marianogoldman@users.noreply.github.com>
2022-03-02 12:38:11 +0000

    style: resolve style guide violations

--- packages/core-forger/source/client.test.ts
+++ packages/core-forger/source/client.test.ts
@@ -72,11 +72,13 @@ describe<{
 		context.client.register(hosts);
 
 		await assert.resolves(() => context.client.broadcastBlock(forgedBlockWithTransactions));
-		assert.true(context.logger.debug.calledWith(
-			`Broadcasting block ${forgedBlockWithTransactions.data.height.toLocaleString()} (${
-				forgedBlockWithTransactions.data.id
-			}) with ${forgedBlockWithTransactions.data.numberOfTransactions} transactions to ${host.hostname}`,
-		));
+		assert.true(
+			context.logger.debug.calledWith(
+				`Broadcasting block ${forgedBlockWithTransactions.data.height.toLocaleString()} (${
+					forgedBlockWithTransactions.data.id
+				}) with ${forgedBlockWithTransactions.data.numberOfTransactions} transactions to ${host.hostname}`,
+			),
+		);
 	});
 
 	it("broadcastBlock should not broadcast block when there is an issue with socket", async (context) => {
@@ -85,9 +87,11 @@ describe<{
 		host.socket = {};
 		await assert.resolves(() => context.client.broadcastBlock(forgedBlockWithTransactions));
 
-		assert.true(context.logger.error.calledWith(
-			`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'this.host.socket.request is not a function'.`,
-		));
+		assert.true(
+			context.logger.error.calledWith(
+				`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'this.host.socket.request is not a function'.`,
+			),
+		);
 	});
 
 	it("broadcastBlock should broadcast valid blocks without error", async (context) => {
@@ -99,10 +103,12 @@ describe<{
 
 		await assert.resolves(() => context.client.broadcastBlock(forgedBlockWithTransactions));
 
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.blocks.postBlock",
-			payload: sinon.match.instanceOf(Buffer),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.blocks.postBlock",
+				payload: sinon.match.instanceOf(Buffer),
+			}),
+		);
 
 		assert.false(context.logger.error.calledOnce);
 	});
@@ -114,9 +120,11 @@ describe<{
 
 		await assert.resolves(() => context.client.broadcastBlock(forgedBlockWithTransactions));
 
-		assert.true(context.logger.error.calledWith(
-			`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'oops'.`,
-		));
+		assert.true(
+			context.logger.error.calledWith(
+				`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'oops'.`,
+			),
+		);
 	});
 
 	it("selectHost should select the first open socket", async (context) => {
@@ -136,23 +144,28 @@ describe<{
 
 		context.client.register(hosts);
 
-		await assert.rejects(() => context.client.selectHost(),
+		await assert.rejects(
+			() => context.client.selectHost(),
 			`${hosts.map((host) => host.hostname).join()} didn't respond. Trying again later.`,
 		);
-		assert.true(context.logger.debug.calledWith(
-			`No open socket connection to any host: ${JSON.stringify(
-				hosts.map((host) => `${host.hostname}:${host.port}`),
-			)}.`,
-		));
+		assert.true(
+			context.logger.debug.calledWith(
+				`No open socket connection to any host: ${JSON.stringify(
+					hosts.map((host) => `${host.hostname}:${host.port}`),
+				)}.`,
+			),
+		);
 	});
 
 	it("getTransactions should call p2p.internal.getUnconfirmedTransactions endpoint", async (context) => {
 		context.client.register([host]);
 		await context.client.getTransactions();
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.internal.getUnconfirmedTransactions",
-			payload: Buffer.from(JSON.stringify({})),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.internal.getUnconfirmedTransactions",
+				payload: Buffer.from(JSON.stringify({})),
+			}),
+		);
 	});
 
 	it("getRound should broadcast internal getRound transaction", async (context) => {
@@ -161,10 +174,12 @@ describe<{
 
 		await context.client.getRound();
 
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.internal.getCurrentRound",
-			payload: Buffer.from(JSON.stringify({})),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.internal.getCurrentRound",
+				payload: Buffer.from(JSON.stringify({})),
+			}),
+		);
 	});
 
 	it("syncWithNetwork should broadcast internal getRound transaction", async (context) => {
@@ -172,10 +187,12 @@ describe<{
 		host.socket._isReady = () => true;
 		await context.client.syncWithNetwork();
 
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.internal.syncBlockchain",
-			payload: Buffer.from(JSON.stringify({})),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.internal.syncBlockchain",
+				payload: Buffer.from(JSON.stringify({})),
+			}),
+		);
 		assert.true(context.logger.debug.calledWith(`Sending wake-up check to relay node ${host.hostname}`));
 	});
 
@@ -187,19 +204,23 @@ describe<{
 
 		await assert.resolves(() => context.client.syncWithNetwork());
 
-		assert.true(context.logger.error.calledWith(
-			`Could not sync check: Request to 127.0.0.1:4000<p2p.internal.syncBlockchain> failed, because of '${errorMessage}'.`,
-		));
+		assert.true(
+			context.logger.error.calledWith(
+				`Could not sync check: Request to 127.0.0.1:4000<p2p.internal.syncBlockchain> failed, because of '${errorMessage}'.`,
+			),
+		);
 	});
 
 	it("getNetworkState should emit internal getNetworkState event", async (context) => {
 		context.client.register([host]);
 		await context.client.getNetworkState();
 
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.internal.getNetworkState",
-			payload: Buffer.from(JSON.stringify({})),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.internal.getNetworkState",
+				payload: Buffer.from(JSON.stringify({})),
+			}),
+		);
 	});
 
 	it("getNetworkState should return valid network state on error", async (context) => {
@@ -220,15 +241,17 @@ describe<{
 
 		await context.client.emitEvent("test-event", data);
 
-		assert.true(nesClient.request.calledWith({
-			path: "p2p.internal.emitEvent",
-			payload: Buffer.from(
-				JSON.stringify({
-					event: "test-event",
-					body: data,
-				}),
-			),
-		}));
+		assert.true(
+			nesClient.request.calledWith({
+				path: "p2p.internal.emitEvent",
+				payload: Buffer.from(
+					JSON.stringify({
+						event: "test-event",
+						body: data,
+					}),
+				),
+			}),
+		);
 	});
 
 	it("emitEvent should not emit events which are not from localhost", async (context) => {
@@ -252,8 +275,6 @@ describe<{
 		const data = { activeDelegates: ["delegate-one"] };
 		await context.client.emitEvent(event, data);
 
-		assert.true(context.logger.error.calledWith(
-			`Failed to emit "${event}" to "${host.hostname}:${host.port}"`,
-		));
+		assert.true(context.logger.error.calledWith(`Failed to emit "${event}" to "${host.hostname}:${host.port}"`));
 	});
 });
