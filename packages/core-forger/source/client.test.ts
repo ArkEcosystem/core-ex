bcf0d7f27a511ddc524e8e91d579bc9b12a286f9
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2022-03-02 07:38:33 -0300

    context

--- packages/core-forger/source/client.test.ts
+++ packages/core-forger/source/client.test.ts
@@ -7,159 +7,157 @@ import { forgedBlockWithTransactions } from "../test/create-block-with-transacti
 
 import { nesClient } from "../test/mocks/nes";
 
-jest.spyOn(Nes, "Client").mockImplementation((url) => nesClient as any);
+// jest.spyOn(Nes, "Client").mockImplementation((url) => nesClient as any);
 
-let app: Application;
-const logger = {
-	error: jest.fn(),
-	debug: jest.fn(),
-};
-
-describe("Client", ({ assert, beforeEach, it, spy, spyFn, stub, stubFn }) => {
-	let client: Client;
-
+describe<{
+	app: Application;
+	client: Client;
+	logger: any;
+}>("Client", ({ assert, beforeEach, it, spy, spyFn, stub, stubFn }) => {
 	const host = { hostname: "127.0.0.1", port: 4000, socket: undefined };
 	const hostIPv6 = { hostname: "::1", port: 4000, socket: undefined };
 	const hosts = [host];
 	const hostsIPv6 = [hostIPv6];
 
 	beforeEach((context) => {
-		app = new Application(new Container.Container());
-		app.bind(Container.Identifiers.LogService).toConstantValue(logger);
+		context.logger = {
+			error: spyFn(),
+			debug: spyFn(),
+		};
 
-		client = app.resolve<Client>(Client);
+		context.app = new Application(new Container.Container());
+		context.app.bind(Container.Identifiers.LogService).toConstantValue(context.logger);
 
-		logger.error.mockReset();
-		logger.debug.mockReset();
+		context.client = context.app.resolve<Client>(Client);
 	});
 
-	it("register should register hosts", async () => {
-		client.register(hosts);
+	it("register should register hosts", async (context) => {
+		context.client.register(hosts);
 
 		const expectedUrl = `ws://${host.hostname}:${host.port}`;
 		const expectedOptions = { ws: { maxPayload: 20971520 } };
 
 		expect(Nes.Client).toHaveBeenCalledWith(expectedUrl, expectedOptions);
-		expect(client.hosts).toEqual([{ ...host, socket: expect.anything() }]);
+		expect(context.client.hosts).toEqual([{ ...host, socket: expect.anything() }]);
 	});
 
-	it("register should register IPv6 hosts", async () => {
-		client.register(hostsIPv6);
+	it("register should register IPv6 hosts", async (context) => {
+		context.client.register(hostsIPv6);
 
 		const expectedUrl = `ws://[${hostIPv6.hostname}]:${hostIPv6.port}`;
 		const expectedOptions = { ws: { maxPayload: 20971520 } };
 
 		expect(Nes.Client).toHaveBeenCalledWith(expectedUrl, expectedOptions);
-		expect(client.hosts).toEqual([{ ...hostIPv6, socket: expect.anything() }]);
+		expect(context.client.hosts).toEqual([{ ...hostIPv6, socket: expect.anything() }]);
 	});
 
-	it("register on error the socket should call logger", () => {
-		client.register(hosts);
+	it("register on error the socket should call logger", (context) => {
+		context.client.register(hosts);
 
 		const fakeError = { message: "Fake Error" };
-		client.hosts[0].socket.onError(fakeError);
+		context.client.hosts[0].socket.onError(fakeError);
 
-		expect(logger.error).toHaveBeenCalledWith("Fake Error");
+		expect(context.logger.error).toHaveBeenCalledWith("Fake Error");
 	});
 
-	it("dispose should call disconnect on all sockets", () => {
-		client.register([host, { hostname: "127.0.0.5", port: 4000 }]);
-		client.dispose();
-		expect(client.hosts[0].socket.disconnect).toHaveBeenCalled();
-		expect(client.hosts[1].socket.disconnect).toHaveBeenCalled();
+	it("dispose should call disconnect on all sockets", (context) => {
+		context.client.register([host, { hostname: "127.0.0.5", port: 4000 }]);
+		context.client.dispose();
+		expect(context.client.hosts[0].socket.disconnect).toHaveBeenCalled();
+		expect(context.client.hosts[1].socket.disconnect).toHaveBeenCalled();
 		expect(nesClient.disconnect).toBeCalled();
 	});
 
-	it("broadcastBlock should log broadcast as debug message", async () => {
-		client.register(hosts);
+	it("broadcastBlock should log broadcast as debug message", async (context) => {
+		context.client.register(hosts);
 
-		await expect(client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
-		expect(logger.debug).toHaveBeenCalledWith(
+		await expect(context.client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+		expect(context.logger.debug).toHaveBeenCalledWith(
 			`Broadcasting block ${forgedBlockWithTransactions.data.height.toLocaleString()} (${
 				forgedBlockWithTransactions.data.id
 			}) with ${forgedBlockWithTransactions.data.numberOfTransactions} transactions to ${host.hostname}`,
 		);
 	});
 
-	it("broadcastBlock should not broadcast block when there is an issue with socket", async () => {
-		client.register(hosts);
+	it("broadcastBlock should not broadcast block when there is an issue with socket", async (context) => {
+		context.client.register(hosts);
 
 		host.socket = {};
-		await expect(client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+		await expect(context.client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
 
-		expect(logger.error).toHaveBeenCalledWith(
+		expect(context.logger.error).toHaveBeenCalledWith(
 			`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'this.host.socket.request is not a function'.`,
 		);
 	});
 
-	it("broadcastBlock should broadcast valid blocks without error", async () => {
-		client.register([host]);
+	it("broadcastBlock should broadcast valid blocks without error", async (context) => {
+		context.client.register([host]);
 
 		nesClient.request.mockResolvedValueOnce({
 			payload: Codecs.postBlock.response.serialize({ status: true, height: 100 }),
 		});
 
-		await expect(client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+		await expect(context.client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
 
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.blocks.postBlock",
 			payload: expect.any(Buffer),
 		});
 
-		expect(logger.error).not.toHaveBeenCalled();
+		expect(context.logger.error).not.toHaveBeenCalled();
 	});
 
-	it("broadcastBlock should not broadcast blocks on socket.request error", async () => {
-		client.register([host]);
+	it("broadcastBlock should not broadcast blocks on socket.request error", async (context) => {
+		context.client.register([host]);
 
 		nesClient.request.mockRejectedValueOnce(new Error("oops"));
 
-		await expect(client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
-		expect(logger.error).toHaveBeenCalledWith(
+		await expect(context.client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+		expect(context.logger.error).toHaveBeenCalledWith(
 			`Broadcast block failed: Request to ${host.hostname}:${host.port}<p2p.blocks.postBlock> failed, because of 'oops'.`,
 		);
 	});
 
-	it("selectHost should select the first open socket", async () => {
+	it("selectHost should select the first open socket", async (context) => {
 		let hosts = [host, host, host, host, host, host, host, host, host, host];
 		hosts[4].socket._isReady = () => true;
 
-		client.register(hosts);
-		client.selectHost();
-		expect((client as any).host).toEqual(hosts[4]);
+		context.client.register(hosts);
+		context.client.selectHost();
+		expect((context.client as any).host).toEqual(hosts[4]);
 	});
 
-	it("selectHost should log debug message when no sockets are open", async () => {
+	it("selectHost should log debug message when no sockets are open", async (context) => {
 		let hosts = [host, host, host, host, host, host, host, host, host, host];
 		hosts.forEach((host) => {
 			host.socket._isReady = () => false;
 		});
 
-		client.register(hosts);
-		await expect(client.selectHost()).rejects.toThrow(
+		context.client.register(hosts);
+		await expect(context.client.selectHost()).rejects.toThrow(
 			`${hosts.map((host) => host.hostname).join()} didn't respond. Trying again later.`,
 		);
-		expect(logger.debug).toHaveBeenCalledWith(
+		expect(context.logger.debug).toHaveBeenCalledWith(
 			`No open socket connection to any host: ${JSON.stringify(
 				hosts.map((host) => `${host.hostname}:${host.port}`),
 			)}.`,
 		);
 	});
 
-	it("getTransactions should call p2p.internal.getUnconfirmedTransactions endpoint", async () => {
-		client.register([host]);
-		await client.getTransactions();
+	it("getTransactions should call p2p.internal.getUnconfirmedTransactions endpoint", async (context) => {
+		context.client.register([host]);
+		await context.client.getTransactions();
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.internal.getUnconfirmedTransactions",
 			payload: Buffer.from(JSON.stringify({})),
 		});
 	});
 
-	it("getRound should broadcast internal getRound transaction", async () => {
-		client.register([host]);
+	it("getRound should broadcast internal getRound transaction", async (context) => {
+		context.client.register([host]);
 		host.socket._isReady = () => true;
 
-		await client.getRound();
+		await context.client.getRound();
 
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.internal.getCurrentRound",
@@ -167,32 +165,32 @@ describe("Client", ({ assert, beforeEach, it, spy, spyFn, stub, stubFn }) => {
 		});
 	});
 
-	it("syncWithNetwork should broadcast internal getRound transaction", async () => {
-		client.register([host]);
+	it("syncWithNetwork should broadcast internal getRound transaction", async (context) => {
+		context.client.register([host]);
 		host.socket._isReady = () => true;
-		await client.syncWithNetwork();
+		await context.client.syncWithNetwork();
 
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.internal.syncBlockchain",
 			payload: Buffer.from(JSON.stringify({})),
 		});
-		expect(logger.debug).toHaveBeenCalledWith(`Sending wake-up check to relay node ${host.hostname}`);
+		expect(context.logger.debug).toHaveBeenCalledWith(`Sending wake-up check to relay node ${host.hostname}`);
 	});
 
-	it("syncWithNetwork should log error message if syncing fails", async () => {
+	it("syncWithNetwork should log error message if syncing fails", async (context) => {
 		const errorMessage = "Fake Error";
 		nesClient.request.mockRejectedValueOnce(new Error(errorMessage));
 		host.socket._isReady = () => true;
-		client.register([host]);
-		await expect(client.syncWithNetwork()).toResolve();
-		expect(logger.error).toHaveBeenCalledWith(
+		context.client.register([host]);
+		await expect(context.client.syncWithNetwork()).toResolve();
+		expect(context.logger.error).toHaveBeenCalledWith(
 			`Could not sync check: Request to 127.0.0.1:4000<p2p.internal.syncBlockchain> failed, because of '${errorMessage}'.`,
 		);
 	});
 
-	it("getNetworkState should emit internal getNetworkState event", async () => {
-		client.register([host]);
-		await client.getNetworkState();
+	it("getNetworkState should emit internal getNetworkState event", async (context) => {
+		context.client.register([host]);
+		await context.client.getNetworkState();
 
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.internal.getNetworkState",
@@ -200,23 +198,23 @@ describe("Client", ({ assert, beforeEach, it, spy, spyFn, stub, stubFn }) => {
 		});
 	});
 
-	it("getNetworkState should return valid network state on error", async () => {
+	it("getNetworkState should return valid network state on error", async (context) => {
 		const errorMessage = "Fake Error";
 		nesClient.request.mockRejectedValueOnce(new Error(errorMessage));
 
-		client.register([host]);
-		const networkState = await client.getNetworkState();
+		context.client.register([host]);
+		const networkState = await context.client.getNetworkState();
 
 		expect(networkState.status).toEqual(NetworkStateStatus.Unknown);
 	});
 
-	it("emitEvent should emit events from localhost", async () => {
+	it("emitEvent should emit events from localhost", async (context) => {
 		host.hostname = "127.0.0.1";
-		client.register([host]);
+		context.client.register([host]);
 
 		const data = { activeDelegates: ["delegate-one"] };
 
-		await client.emitEvent("test-event", data);
+		await context.client.emitEvent("test-event", data);
 
 		expect(nesClient.request).toHaveBeenCalledWith({
 			path: "p2p.internal.emitEvent",
@@ -229,27 +227,29 @@ describe("Client", ({ assert, beforeEach, it, spy, spyFn, stub, stubFn }) => {
 		});
 	});
 
-	it("emitEvent should not emit events which are not from localhost", async () => {
+	it("emitEvent should not emit events which are not from localhost", async (context) => {
 		host.hostname = "127.0.0.2";
-		client.register([host]);
+		context.client.register([host]);
 
 		const data = { activeDelegates: ["delegate-one"] };
-		await client.emitEvent("test-event", data);
+		await context.client.emitEvent("test-event", data);
 
-		expect(logger.error).toHaveBeenCalledWith("emitEvent: unable to find any local hosts.");
+		expect(context.logger.error).toHaveBeenCalledWith("emitEvent: unable to find any local hosts.");
 	});
 
-	it("emitEvent should log error if emitting fails", async () => {
+	it("emitEvent should log error if emitting fails", async (context) => {
 		const errorMessage = "Fake Error";
 		nesClient.request.mockRejectedValueOnce(new Error(errorMessage));
 
 		host.hostname = "127.0.0.1";
-		client.register([host]);
+		context.client.register([host]);
 
 		const event = "test-event";
 		const data = { activeDelegates: ["delegate-one"] };
-		await client.emitEvent(event, data);
+		await context.client.emitEvent(event, data);
 
-		expect(logger.error).toHaveBeenCalledWith(`Failed to emit "${event}" to "${host.hostname}:${host.port}"`);
+		expect(context.logger.error).toHaveBeenCalledWith(
+			`Failed to emit "${event}" to "${host.hostname}:${host.port}"`,
+		);
 	});
 });
