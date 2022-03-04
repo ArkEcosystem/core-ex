0ece7465f12ee68fe7f8f005ab1fa7c5d5259087
faustbrian <faustbrian@users.noreply.github.com>
2022-03-04 14:53:41 +0000

    style: resolve style guide violations

--- packages/core-transaction-pool-api/source/handlers/submit.ts
+++ packages/core-transaction-pool-api/source/handlers/submit.ts
@@ -4,20 +4,20 @@ import { Contracts, Identifiers } from "packages/core-contracts/distribution";
 
 @injectable()
 export class SubmitTransactionHandler {
-    @inject(Identifiers.TransactionPoolProcessor)
-    private readonly processor!: Contracts.TransactionPool.Processor;
+	@inject(Identifiers.TransactionPoolProcessor)
+	private readonly processor!: Contracts.TransactionPool.Processor;
 
 	public async invoke(request: FastifyRequest): Promise<object> {
-        const result = await this.processor.process((request.body as any).transactions);
+		const result = await this.processor.process((request.body as any).transactions);
 
 		return {
-            data: {
-                accept: result.accept,
-                broadcast: result.broadcast,
-                excess: result.excess,
-                invalid: result.invalid,
-            },
-            errors: result.errors,
-        };
+			data: {
+				accept: result.accept,
+				broadcast: result.broadcast,
+				excess: result.excess,
+				invalid: result.invalid,
+			},
+			errors: result.errors,
+		};
 	}
 }
