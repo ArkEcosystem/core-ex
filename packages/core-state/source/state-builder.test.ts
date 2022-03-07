63c16ca63f680482dc972ef0c1cd808a7de8af50
Josip Crnković <jcrnkovic95@gmail.com>
2022-03-07 11:05:38 +0100

    use `context` for `state-builder.test.ts`

--- packages/core-state/source/state-builder.test.ts
+++ packages/core-state/source/state-builder.test.ts
@@ -1,16 +1,10 @@
-import { Enums, Utils } from "@arkecosystem/core-kernel";
+import { Application, Enums, Utils } from "@arkecosystem/core-kernel";
 import { StateBuilder } from "./state-builder";
 import { WalletRepository } from "./wallets";
-import { Sandbox, describe } from "@arkecosystem/core-test-framework";
+import { describe } from "@arkecosystem/core-test-framework";
 import { Managers } from "@arkecosystem/crypto";
 import { setUp, setUpDefaults } from "../test/setup";
 import { SinonSpy } from "sinon";
-
-let stateBuilder: StateBuilder;
-let getBlockRewardsSpy: SinonSpy;
-let getSentTransactionSpy: SinonSpy;
-let getRegisteredHandlersSpy: SinonSpy;
-let dispatchSpy: SinonSpy;
 
 const getBlockRewardsDefault = setUpDefaults.getBlockRewards[0];
 const getSentTransactionDefault = setUpDefaults.getSentTransaction[0];
@@ -18,12 +12,6 @@ const getSentTransactionDefault = setUpDefaults.getSentTransaction[0];
 const generatorKey = getBlockRewardsDefault.generatorPublicKey;
 const senderKey = getSentTransactionDefault.senderPublicKey;
 
-let sandbox: Sandbox;
-
-let loggerWarningSpy: SinonSpy;
-let loggerInfoSpy: SinonSpy;
-
-let walletRepo: WalletRepository;
 let restoreDefaultSentTransactions: () => void;
 
 const saveDefaultTransactions = (): (() => void) => {
@@ -31,125 +19,141 @@ const saveDefaultTransactions = (): (() => void) => {
 	return () => (setUpDefaults.getSentTransaction = saveTransaction);
 };
 
-describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
-	beforeAll(async () => {
-		const initialEnv = await setUp();
+describe<{
+	app: Application;
+	walletRepo: WalletRepository;
+	stateBuilder: StateBuilder;
+	getBlockRewardsSpy: SinonSpy;
+	getSentTransactionSpy: SinonSpy;
+	getRegisteredHandlersSpy: SinonSpy;
+	dispatchSpy: SinonSpy;
+	loggerWarningSpy: SinonSpy;
+	loggerInfoSpy: SinonSpy;
+}>("StateBuilder", ({ it, beforeAll, afterEach, beforeEach, assert }) => {
+	beforeAll(async (context) => {
+		const env = await setUp();
 
-		walletRepo = initialEnv.walletRepo;
-		stateBuilder = initialEnv.stateBuilder;
-		sandbox = initialEnv.sandbox;
+		context.walletRepo = env.walletRepo;
+		context.stateBuilder = env.stateBuilder;
+		context.app = env.sandbox.app;
 
-		getBlockRewardsSpy = initialEnv.spies.getBlockRewardsSpy;
-		getSentTransactionSpy = initialEnv.spies.getSentTransactionSpy;
-		getRegisteredHandlersSpy = initialEnv.spies.getRegisteredHandlersSpy;
-		dispatchSpy = initialEnv.spies.dispatchSpy;
-		loggerWarningSpy = initialEnv.spies.logger.warning;
-		loggerInfoSpy = initialEnv.spies.logger.info;
+		context.getBlockRewardsSpy = env.spies.getBlockRewardsSpy;
+		context.getSentTransactionSpy = env.spies.getSentTransactionSpy;
+		context.getRegisteredHandlersSpy = env.spies.getRegisteredHandlersSpy;
+		context.dispatchSpy = env.spies.dispatchSpy;
+		context.loggerWarningSpy = env.spies.logger.warning;
+		context.loggerInfoSpy = env.spies.logger.info;
 
 		restoreDefaultSentTransactions = saveDefaultTransactions();
 	});
 
-	beforeEach(() => {
-		walletRepo.reset();
-
-		loggerWarningSpy.resetHistory();
-		dispatchSpy.resetHistory();
-
-		sandbox.app.config("crypto.exceptions.negativeBalances", {});
+	beforeEach((context) => {
+		context.app.config("crypto.exceptions.negativeBalances", {});
 
 		restoreDefaultSentTransactions();
 
 		// sender wallet balance should always be enough for default transactions (unless it is overridden)
-		const wallet = walletRepo.findByPublicKey(senderKey);
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setBalance(Utils.BigNumber.make(100000));
 	});
 
-	it("should call block repository to get initial block rewards", async () => {
-		await stateBuilder.run();
+	afterEach((context) => {
+		context.walletRepo.reset();
 
-		assert.true(getBlockRewardsSpy.called);
+		context.getBlockRewardsSpy.resetHistory();
+		context.getSentTransactionSpy.resetHistory();
+		context.getRegisteredHandlersSpy.resetHistory();
+		context.dispatchSpy.resetHistory();
+		context.loggerWarningSpy.resetHistory();
+		context.loggerInfoSpy.resetHistory();
 	});
 
-	it("should get registered handlers", async () => {
-		await stateBuilder.run();
+	it("should call block repository to get initial block rewards", async (context) => {
+		await context.stateBuilder.run();
 
-		assert.true(getRegisteredHandlersSpy.called);
+		assert.true(context.getBlockRewardsSpy.called);
 	});
 
-	it("should get sent transactions", async () => {
-		await stateBuilder.run();
+	it("should get registered handlers", async (context) => {
+		await context.stateBuilder.run();
 
-		assert.true(getSentTransactionSpy.called);
+		assert.true(context.getRegisteredHandlersSpy.called);
 	});
 
-	it("should apply block rewards to generator wallet", async () => {
-		const wallet = walletRepo.findByPublicKey(generatorKey);
+	it("should get sent transactions", async (context) => {
+		await context.stateBuilder.run();
+
+		assert.true(context.getSentTransactionSpy.called);
+	});
+
+	it("should apply block rewards to generator wallet", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(generatorKey);
 		wallet.setBalance(Utils.BigNumber.ZERO);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 		const expectedBalance = wallet.getBalance().plus(getBlockRewardsDefault.rewards);
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.equal(wallet.getBalance(), expectedBalance);
 	});
 
-	it("should apply the transaction data to the sender", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should apply the transaction data to the sender", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setBalance(Utils.BigNumber.make(80000));
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
 		const expectedBalance = wallet
 			.getBalance()
 			.minus(getSentTransactionDefault.amount)
 			.minus(getSentTransactionDefault.fee);
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.equal(wallet.getNonce(), getSentTransactionDefault.nonce);
 		assert.equal(wallet.getBalance(), expectedBalance);
 	});
 
-	it("should fail if any wallet balance is negative and not whitelisted", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should fail if any wallet balance is negative and not whitelisted", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setBalance(Utils.BigNumber.make(-80000));
 		wallet.setPublicKey(senderKey);
 
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.true(
-			loggerWarningSpy.calledWith(
+			context.loggerWarningSpy.calledWith(
 				"Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-135555'",
 			),
 		);
-		assert.false(dispatchSpy.called);
+		assert.false(context.dispatchSpy.called);
 	});
 
-	it("should not fail for negative genesis wallet balances", async () => {
+	it("should not fail for negative genesis wallet balances", async (context) => {
 		const genesisPublicKeys: string[] = Managers.configManager
 			.get("genesisBlock.transactions")
 			.reduce((acc, curr) => [...acc, curr.senderPublicKey], []);
 
-		const wallet = walletRepo.findByPublicKey(genesisPublicKeys[0]);
+		const wallet = context.walletRepo.findByPublicKey(genesisPublicKeys[0]);
 		wallet.setBalance(Utils.BigNumber.make(-80000));
 		wallet.setPublicKey(genesisPublicKeys[0]);
 
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
-		assert.false(loggerWarningSpy.called);
-		assert.true(dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
+		assert.false(context.loggerWarningSpy.called);
+		assert.true(context.dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
 	});
 
-	it("should not fail if the publicKey is whitelisted", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should not fail if the publicKey is whitelisted", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setNonce(getSentTransactionDefault.nonce);
 		const allowedWalletNegativeBalance = Utils.BigNumber.make(5555);
 		wallet.setBalance(allowedWalletNegativeBalance);
 		wallet.setPublicKey(senderKey);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
 		const balance: Record<string, Record<string, string>> = {
 			[senderKey]: {
@@ -157,22 +161,22 @@ describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
 			},
 		};
 
-		sandbox.app.config("crypto.exceptions.negativeBalances", balance);
+		context.app.config("crypto.exceptions.negativeBalances", balance);
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
-		assert.false(loggerWarningSpy.called);
-		assert.true(dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
+		assert.false(context.loggerWarningSpy.called);
+		assert.true(context.dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
 	});
 
-	it("should fail if the whitelisted key doesn't have the allowed negative balance", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should fail if the whitelisted key doesn't have the allowed negative balance", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setNonce(getSentTransactionDefault.nonce);
 		wallet.setBalance(Utils.BigNumber.make(-90000));
 		wallet.setPublicKey(senderKey);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
 		const balance: Record<string, Record<string, string>> = {
 			[senderKey]: {
@@ -180,24 +184,24 @@ describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
 			},
 		};
 
-		sandbox.app.config("crypto.exceptions.negativeBalances", balance);
+		context.app.config("crypto.exceptions.negativeBalances", balance);
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.true(
-			loggerWarningSpy.calledWith("Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-90000'"),
+			context.loggerWarningSpy.calledWith("Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-90000'"),
 		);
-		assert.false(dispatchSpy.called);
+		assert.false(context.dispatchSpy.called);
 	});
 
-	it("should not fail if the whitelisted key has the allowed negative balance", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should not fail if the whitelisted key has the allowed negative balance", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setNonce(getSentTransactionDefault.nonce);
 		wallet.setBalance(Utils.BigNumber.make(-90000));
 		wallet.setPublicKey(senderKey);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
 		const balance: Record<string, Record<string, string>> = {
 			[senderKey]: {
@@ -205,37 +209,37 @@ describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
 			},
 		};
 
-		sandbox.app.config("crypto.exceptions.negativeBalances", balance);
+		context.app.config("crypto.exceptions.negativeBalances", balance);
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
-		assert.false(loggerWarningSpy.called);
-		assert.true(dispatchSpy.called);
+		assert.false(context.loggerWarningSpy.called);
+		assert.true(context.dispatchSpy.called);
 	});
 
-	it("should not fail if delegates vote balance isn't below 0", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should not fail if delegates vote balance isn't below 0", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setBalance(Utils.BigNumber.ZERO);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 		wallet.setAttribute("delegate.voteBalance", Utils.BigNumber.make(100));
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
-		assert.false(loggerWarningSpy.called);
-		assert.true(dispatchSpy.called);
+		assert.false(context.loggerWarningSpy.called);
+		assert.true(context.dispatchSpy.called);
 	});
 
-	it("should fail if the wallet has no public key", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should fail if the wallet has no public key", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setNonce(getSentTransactionDefault.nonce);
 		wallet.setBalance(Utils.BigNumber.make(-90000));
 		// @ts-ignore
 		wallet.publicKey = undefined;
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 
 		const balance: Record<string, Record<string, string>> = {
 			[senderKey]: {
@@ -243,42 +247,42 @@ describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
 			},
 		};
 
-		sandbox.app.config("crypto.exceptions.negativeBalances", balance);
+		context.app.config("crypto.exceptions.negativeBalances", balance);
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.true(
-			loggerWarningSpy.calledWith("Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-90000'"),
+			context.loggerWarningSpy.calledWith("Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-90000'"),
 		);
-		assert.false(dispatchSpy.called);
+		assert.false(context.dispatchSpy.called);
 	});
 
-	it("should emit an event when the builder is finished", async () => {
-		await stateBuilder.run();
+	it("should emit an event when the builder is finished", async (context) => {
+		await context.stateBuilder.run();
 
-		assert.true(dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
+		assert.true(context.dispatchSpy.calledWith(Enums.StateEvent.BuilderFinished));
 	});
 
-	it("should exit app if any vote balance is negative", async () => {
-		const wallet = walletRepo.findByPublicKey(senderKey);
+	it("should exit app if any vote balance is negative", async (context) => {
+		const wallet = context.walletRepo.findByPublicKey(senderKey);
 		wallet.setBalance(Utils.BigNumber.ZERO);
-		walletRepo.index(wallet);
+		context.walletRepo.index(wallet);
 		wallet.setAttribute("delegate.voteBalance", Utils.BigNumber.make(-100));
 
 		setUpDefaults.getSentTransaction = [];
 
-		await stateBuilder.run();
+		await context.stateBuilder.run();
 
 		assert.true(
-			loggerWarningSpy.calledWith(
+			context.loggerWarningSpy.calledWith(
 				"Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative vote balance of '-100'",
 			),
 		);
 	});
 
-	it("should capitalise registered handlers", async () => {
+	it("should capitalise registered handlers", async (context) => {
 		setUpDefaults.getRegisteredHandlers = [
 			{
 				getConstructor: () => ({
@@ -290,8 +294,8 @@ describe("StateBuilder", ({ it, beforeAll, beforeEach, assert }) => {
 
 		setUpDefaults.getSentTransaction = [];
 
-		await assert.resolves(() => stateBuilder.run());
+		await assert.resolves(() => context.stateBuilder.run());
 
-		assert.true(loggerInfoSpy.calledWith(`State Generation - Step 3 of 4: Test v1`));
+		assert.true(context.loggerInfoSpy.calledWith(`State Generation - Step 3 of 4: Test v1`));
 	});
 });
