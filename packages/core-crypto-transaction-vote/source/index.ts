8599754ae5102b06386658b50546e57e9bd076ff
Brian Faust <hello@basecode.sh>
2022-03-08 06:40:28 +0200

    Revert "wip"
    This reverts commit 2c93e992f4e6bb3405e74fce2cf16b3d806a4be8.

---
+++ packages/core-crypto-transaction-vote/source/index.ts
@@ -0,0 +1,19 @@
+import { Container } from "@arkecosystem/core-container";
+import { BINDINGS } from "@arkecosystem/core-crypto-contracts";
+import { TransactionRegistry } from "@arkecosystem/core-crypto-transaction";
+import { Providers } from "@arkecosystem/core-kernel";
+
+import { One } from "./versions/1";
+import { Two } from "./versions/2";
+
+export * from "./builder";
+
+@Container.injectable()
+export class ServiceProvider extends Providers.ServiceProvider {
+	public async register(): Promise<void> {
+		const registry: TransactionRegistry = this.app.get(BINDINGS.Transaction.Registry);
+
+		registry.registerTransactionType(One);
+		registry.registerTransactionType(Two);
+	}
+}
