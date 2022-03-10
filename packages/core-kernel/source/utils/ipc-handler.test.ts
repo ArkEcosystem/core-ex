3884a91b939f8e45c80150362910c18fc9426a5e
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2022-03-10 11:06:47 -0300

    migrate tests to uvu

--- packages/core-kernel/source/utils/ipc-handler.test.ts
+++ packages/core-kernel/source/utils/ipc-handler.test.ts
@@ -1,40 +1,51 @@
-import { IpcHandler } from "../../../../packages/core-kernel/source/utils/ipc-handler";
+import { describe } from "../../../core-test-framework";
+
+import { IpcHandler } from "./ipc-handler";
 
 type MyRpcInterface = {
 	myRpcActionMethod(a: number, b: number): void;
 	myRpcRequestMethod(a: number, b: number): Promise<string>;
 };
 
-describe("IpcHandler.handleAction", () => {
+describe("IpcHandler", ({ it, spy }) => {
 	it("should call method on process message", () => {
 		try {
-			const myHandler = { myRpcActionMethod: jest.fn() };
+			const myHandler = { myRpcActionMethod: () => undefined };
+			const myRpcActionMethodSpy = spy(myHandler, "myRpcActionMethod");
 			const ipcHandler = new IpcHandler<MyRpcInterface>(myHandler as any);
 			ipcHandler.handleAction("myRpcActionMethod");
 			process.listeners("message").forEach((l) => {
 				l({ method: "myRpcActionMethod", args: [1, 2] }, null);
 			});
-			expect(myHandler.myRpcActionMethod).toBeCalledWith(1, 2);
+			myRpcActionMethodSpy.calledWith(1, 2);
 		} finally {
 			process.removeAllListeners("message");
 		}
 	});
-});
 
-describe("IpcHandler.handleRequest", () => {
 	it("should call method, await result, and send result back", async () => {
 		try {
-			const myHandler = { myRpcRequestMethod: jest.fn() };
-			myHandler.myRpcRequestMethod.mockResolvedValueOnce("hello");
+			const myHandler = {
+				myRpcRequestMethod: () => "hello",
+			};
+			const myRpcRequestMethodSpy = spy(myHandler, "myRpcRequestMethod");
+
 			const ipcHandler = new IpcHandler<MyRpcInterface>(myHandler as any);
+
 			ipcHandler.handleRequest("myRpcRequestMethod");
-			process.send = jest.fn();
+
+			process.send = () => true;
+			const processSendSpy = spy(process, "send");
+
 			process.listeners("message").forEach((l) => {
 				l({ id: 1, method: "myRpcRequestMethod", args: [1, 2] }, null);
 			});
-			await myHandler.myRpcRequestMethod.mock.results[0].value;
-			expect(myHandler.myRpcRequestMethod).toBeCalledWith(1, 2);
-			expect(process.send).toBeCalledWith({ id: 1, result: "hello" });
+
+			// @ts-ignore
+			await myHandler.myRpcRequestMethod.getCall(0).proxy();
+
+			myRpcRequestMethodSpy.calledWith(1, 2);
+			processSendSpy.calledWith({ id: 1, result: "hello" });
 		} finally {
 			process.removeAllListeners("message");
 		}
@@ -42,19 +53,31 @@ describe("IpcHandler.handleRequest", () => {
 
 	it("should call method, await result, catch error and send error back", async () => {
 		try {
-			const myHandler = { myRpcRequestMethod: jest.fn() };
-			myHandler.myRpcRequestMethod.mockRejectedValueOnce(new Error("hello"));
+			const myHandler = {
+				myRpcRequestMethod: () => {
+					throw new Error("hello");
+				},
+			};
+			const myRpcRequestMethodSpy = spy(myHandler, "myRpcRequestMethod");
+
 			const ipcHandler = new IpcHandler<MyRpcInterface>(myHandler as any);
+
 			ipcHandler.handleRequest("myRpcRequestMethod");
-			process.send = jest.fn();
+
+			process.send = () => true;
+			const processSendSpy = spy(process, "send");
+
 			process.listeners("message").forEach((l) => {
 				l({ id: 1, method: "myRpcRequestMethod", args: [1, 2] }, null);
 			});
+
 			try {
-				await myHandler.myRpcRequestMethod.mock.results[0].value;
+				// @ts-ignore
+				await myHandler.myRpcRequestMethod.getCall(0).proxy();
 			} catch (error) {}
-			expect(myHandler.myRpcRequestMethod).toBeCalledWith(1, 2);
-			expect(process.send).toBeCalledWith({ id: 1, error: "hello" });
+
+			myRpcRequestMethodSpy.calledWith(1, 2);
+			processSendSpy.calledWith({ id: 1, error: "hello" });
 		} finally {
 			process.removeAllListeners("message");
 		}
