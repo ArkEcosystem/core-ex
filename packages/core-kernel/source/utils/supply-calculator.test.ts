5b43d5a13a93e50ce004588bcac7bbcce4394c52
Brian Faust <hello@basecode.sh>
2022-03-10 17:55:25 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/core-kernel/source/utils/supply-calculator.test.ts
+++ packages/core-kernel/source/utils/supply-calculator.test.ts
@@ -1,6 +1,6 @@
 import { Interfaces, Managers, Utils } from "@arkecosystem/crypto";
-import { describe } from "../../../core-test-framework";
 
+import { describe } from "../../../core-test-framework";
 import { calculate } from "./supply-calculator";
 
 const toString = (value) => Utils.BigNumber.make(value).toFixed();
@@ -8,7 +8,7 @@ const toString = (value) => Utils.BigNumber.make(value).toFixed();
 describe<{
 	config: Interfaces.NetworkConfig;
 	mockConfig: any;
-}>("Supply calculator", ({ afterEach, beforeEach, assert, it }) => {
+}>("Supply calculator", ({ afterEach, beforeEach, assert, each, it }) => {
 	beforeEach((context) => {
 		context.config = Managers.configManager.all();
 
@@ -30,61 +30,52 @@ describe<{
 		assert.is(calculate(1), toString(context.mockConfig.genesisBlock.totalAmount));
 	});
 
-	for (const height of [0, 5, 100, 2000, 4000, 8000]) {
-		it(`at height ${height} should calculate the genesis supply without milestone`, (context) => {
-			const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
-			assert.is(calculate(height), toString(genesisSupply + height * context.mockConfig.milestones[0].reward));
-		});
-	}
+	each("at height %s should calculate the genesis supply without milestone", ({ context, dataset }) => {
+		const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
+		assert.is(calculate(dataset), toString(genesisSupply + dataset * context.mockConfig.milestones[0].reward));
+	}, [0, 5, 100, 2000, 4000, 8000]);
 
-	for (const height of [0, 2000, 4000, 8000, 16000]) {
-		it(`at height ${height} should calculate the genesis supply with one milestone`, (context) => {
-			context.mockConfig.milestones.push({ height: 8000, reward: 3 });
+	each("at height %s should calculate the genesis supply with one milestone", ({ context, dataset }) => {
+		context.mockConfig.milestones.push({ height: 8000, reward: 3 });
 
-			const reward = (current) => {
-				if (current < 8000) {
-					return current * 2;
-				}
+		const reward = (current) => {
+			if (current < 8000) {
+				return current * 2;
+			}
 
-				return 7999 * 2 + (current - 7999) * 3;
-			};
+			return 7999 * 2 + (current - 7999) * 3;
+		};
 
-			const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
+		const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
 
-			assert.is(calculate(height), toString(genesisSupply + reward(height)));
-		});
-	}
+		assert.is(calculate(dataset), toString(genesisSupply + reward(dataset)));
+	}, [0, 2000, 4000, 8000, 16_000]);
 
-	for (const height of [0, 4000, 8000, 12000, 16000, 20000, 32000, 48000, 64000, 128000]) {
-		it(`at height ${height} should calculate the genesis supply with four milestones`, (context) => {
-			context.mockConfig.milestones.push({ height: 8000, reward: 4 });
-			context.mockConfig.milestones.push({ height: 16000, reward: 5 });
-			context.mockConfig.milestones.push({ height: 32000, reward: 10 });
-			context.mockConfig.milestones.push({ height: 64000, reward: 15 });
+	each("at height %s should calculate the genesis supply with four milestones", ({ context, dataset }) => {
+		context.mockConfig.milestones.push({ height: 8000, reward: 4 }, { height: 16_000, reward: 5 }, { height: 32_000, reward: 10 }, { height: 64_000, reward: 15 });
 
-			const reward = (current) => {
-				if (current < 8000) {
-					return current * 2;
-				}
+		const reward = (current) => {
+			if (current < 8000) {
+				return current * 2;
+			}
 
-				if (current < 16000) {
-					return reward(7999) + (current - 7999) * 4;
-				}
+			if (current < 16_000) {
+				return reward(7999) + (current - 7999) * 4;
+			}
 
-				if (current < 32000) {
-					return reward(15999) + (current - 15999) * 5;
-				}
+			if (current < 32_000) {
+				return reward(15_999) + (current - 15_999) * 5;
+			}
 
-				if (current < 64000) {
-					return reward(31999) + (current - 31999) * 10;
-				}
+			if (current < 64_000) {
+				return reward(31_999) + (current - 31_999) * 10;
+			}
 
-				return reward(63999) + (current - 63999) * 15;
-			};
+			return reward(63_999) + (current - 63_999) * 15;
+		};
 
-			const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
+		const genesisSupply = context.mockConfig.genesisBlock.totalAmount;
 
-			assert.is(calculate(height), toString(genesisSupply + reward(height)));
-		});
-	}
+		assert.is(calculate(dataset), toString(genesisSupply + reward(dataset)));
+	}, [0, 4000, 8000, 12_000, 16_000, 20_000, 32_000, 48_000, 64_000, 128_000]);
 });
