72878773215acada76ad20062aa3c3bea6965fe7
Brian Faust <hello@basecode.sh>
2022-03-10 10:41:38 +0200

    wip
    Signed-off-by: Brian Faust <hello@basecode.sh>

--- packages/core-transaction-pool/source/expiration-service.ts
+++ packages/core-transaction-pool/source/expiration-service.ts
@@ -1,21 +1,13 @@
-import { inject, injectable, tagged } from "@arkecosystem/core-container";
+import { inject, injectable } from "@arkecosystem/core-container";
 import { Contracts, Identifiers } from "@arkecosystem/core-contracts";
-import { Providers, Utils as AppUtils } from "@arkecosystem/core-kernel";
 
 @injectable()
 export class ExpirationService implements Contracts.TransactionPool.ExpirationService {
 	@inject(Identifiers.Application)
 	public readonly app!: Contracts.Kernel.Application;
 
-	@inject(Identifiers.PluginConfiguration)
-	@tagged("plugin", "core-transaction-pool")
-	private readonly pluginConfiguration!: Providers.PluginConfiguration;
-
 	@inject(Identifiers.StateStore)
 	private readonly stateStore!: Contracts.State.StateStore;
-
-	@inject(Identifiers.Cryptography.Time.Slots)
-	private readonly slots: Contracts.Crypto.Slots;
 
 	public canExpire(transaction: Contracts.Crypto.ITransaction): boolean {
 		return !!transaction.data.expiration;
@@ -30,17 +22,6 @@ export class ExpirationService implements Contracts.TransactionPool.ExpirationSe
 	}
 
 	public async getExpirationHeight(transaction: Contracts.Crypto.ITransaction): Promise<number> {
-		if (transaction.data.version && transaction.data.version >= 2) {
-			AppUtils.assert.defined<number>(transaction.data.expiration);
-			return transaction.data.expiration;
-		} else {
-			const currentHeight: number = this.stateStore.getLastHeight();
-
-			const createdSecondsAgo: number = this.slots.getTime() - transaction.data.timestamp;
-			const createdBlocksAgo: number = await this.slots.getSlotNumber(createdSecondsAgo);
-			const maxTransactionAge: number = this.pluginConfiguration.getRequired<number>("maxTransactionAge");
-
-			return Math.floor(currentHeight - createdBlocksAgo + maxTransactionAge);
-		}
+		return transaction.data.expiration;
 	}
 }
