a07cde69a5333c165b5f28717bb49b59168d17fb
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2022-03-14 08:50:10 -0300

    separate sign from verification

--- packages/core-crypto-signature-schnorr-legacy/source/signature.test.ts
+++ packages/core-crypto-signature-schnorr-legacy/source/signature.test.ts
@@ -3,14 +3,21 @@ import { describe } from "../../core-test-framework/source";
 import { Signature } from "./signature";
 
 describe("Signature", ({ assert, it }) => {
-	it("should sign and verify", async () => {
+	it("should sign", async () => {
+		assert.equal(
+			await new Signature().sign(
+				Buffer.from("64726e3da8", "hex"),
+				Buffer.from("814857ce48e291893feab95df02e1dbf7ad3994ba46f247f77e4eefd5d8734a2", "hex"),
+			),
+			"4ae81f14a2b8a1297c18b587a4502d70999343d932e8be827d3e41d545ab71f53ab2a6371838f8fb3ede8f573c140802dc400e1b99dbc086a91b4788aba98d44",
+		);
+	});
+
+	it("should verify", async () => {
 		assert.true(
 			await new Signature().verify(
 				Buffer.from(
-					await new Signature().sign(
-						Buffer.from("64726e3da8", "hex"),
-						Buffer.from("814857ce48e291893feab95df02e1dbf7ad3994ba46f247f77e4eefd5d8734a2", "hex"),
-					),
+					"4ae81f14a2b8a1297c18b587a4502d70999343d932e8be827d3e41d545ab71f53ab2a6371838f8fb3ede8f573c140802dc400e1b99dbc086a91b4788aba98d44",
 					"hex",
 				),
 				Buffer.from("64726e3da8", "hex"),
