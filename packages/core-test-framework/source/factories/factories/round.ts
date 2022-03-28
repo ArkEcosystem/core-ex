9ffd34b9cbc38e23070bcfdc461c5b8b636d02eb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-03-28 09:02:14 +0000

    Round factory

--- packages/core-test-framework/source/factories/factories/round.ts
+++ packages/core-test-framework/source/factories/factories/round.ts
@@ -1,25 +1,41 @@
-// import { Wallets } from "@arkecosystem/core-state";
-// import { BigNumber } from "@arkecosystem/utils";
+import { Contracts, Identifiers } from "@arkecosystem/core-contracts";
+import { Wallets } from "@arkecosystem/core-state";
+import { BigNumber } from "@arkecosystem/utils";
 
-// import passphrases from "../../internal/passphrases.json";
-// import { knownAttributes } from "../../internal/wallet-attributes";
+import passphrases from "../../internal/passphrases.json";
+import { knownAttributes } from "../../internal/wallet-attributes";
 import { FactoryBuilder } from "../factory-builder";
 
-export const registerRoundFactory = (factory: FactoryBuilder): void => {
-	// factory.set("Round", ({ options }) => {
-	// 	const publicKeys: string[] =
-	// 		options.publicKeys ||
-	// 		passphrases.map((passphrase: string) => this.publicKeyFactory.fromMnemonic(passphrase));
-	// 	return publicKeys.map((publicKey: string, index: number) => {
-	// 		const wallet = new Wallets.Wallet(this.addressFactory.fromPublicKey(publicKey), knownAttributes);
-	// 		wallet.setPublicKey(publicKey);
-	// 		wallet.setAttribute("validator", {
-	// 			rank: undefined,
-	// 			round: options.round || 1,
-	// 			username: `genesis_${index + 1}`,
-	// 			voteBalance: BigNumber.make("300000000000000"),
-	// 		});
-	// 		return wallet;
-	// 	});
-	// });
+export const registerRoundFactory = (factory: FactoryBuilder, app: Contracts.Kernel.Application): void => {
+	factory.set("Round", async ({ options }) => {
+		const publicKeys: string[] =
+			options.publicKeys ||
+			(await Promise.all(
+				passphrases.map(
+					async (passphrase: string) =>
+						await app
+							.get<Contracts.Crypto.IPublicKeyFactory>(Identifiers.Cryptography.Identity.PublicKeyFactory)
+							.fromMnemonic(passphrase),
+				),
+			));
+
+		return Promise.all(
+			publicKeys.map(async (publicKey: string, index: number) => {
+				const wallet = new Wallets.Wallet(
+					await app
+						.get<Contracts.Crypto.IAddressFactory>(Identifiers.Cryptography.Identity.AddressFactory)
+						.fromPublicKey(publicKey),
+					knownAttributes,
+				);
+				wallet.setPublicKey(publicKey);
+				wallet.setAttribute("validator", {
+					rank: undefined,
+					round: options.round || 1,
+					username: `genesis_${index + 1}`,
+					voteBalance: BigNumber.make("300000000000000"),
+				});
+				return wallet;
+			}),
+		);
+	});
 };
