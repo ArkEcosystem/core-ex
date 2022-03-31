4dc37b01954fbb501c3dc79c953545f07d430aa0
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-03-31 12:38:22 +0000

    Add network generate package

---
+++ packages/core-network-generate/source/index.ts
@@ -0,0 +1,11 @@
+import { Providers } from "@arkecosystem/core-kernel";
+
+export class ServiceProvider extends Providers.ServiceProvider {
+	public async register(): Promise<void> {
+		//
+	}
+
+	public async boot(): Promise<void> {
+		//
+	}
+}
