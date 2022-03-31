117eaf3757625200797df6889854b820097b3237
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-03-31 14:48:06 +0000

    Test

--- packages/core-network-generate/source/generator.test.ts
+++ packages/core-network-generate/source/generator.test.ts
@@ -4,7 +4,6 @@ import fs from "fs-extra";
 import { join } from "path";
 
 import { describe } from "../../core-test-framework";
-// import { Options } from "./contracts";
 import { NetworkGenerator } from "./generator";
 
 describe<{
@@ -55,6 +54,141 @@ describe<{
 
 		ensureDirSync.calledWith(configCore);
 
+		writeJSONSync.calledTimes(5);
+		writeFileSync.calledOnce();
+
+		writeJSONSync.calledWith(
+			match("crypto.json"),
+			match({
+				genesisBlock: {
+					blockSignature: match.string,
+					generatorPublicKey: match.string,
+					height: 1,
+					id: match.string,
+					numberOfTransactions: 153,
+					payloadHash: match.string,
+					payloadLength: 4896,
+					previousBlock: "0000000000000000000000000000000000000000000000000000000000000000",
+					reward: BigNumber.ZERO,
+					timestamp: match.number,
+					totalAmount: BigNumber.make("12499999999999986"),
+					totalFee: BigNumber.ZERO,
+					transactions: match.array,
+					version: 1,
+				},
+				milestones: [
+					match({
+						activeValidators: 51,
+						address: match.object,
+						block: match.object,
+						blockTime: 9,
+						epoch: match.string,
+						height: 1,
+						multiPaymentLimit: 256,
+						reward: "0", // TODO: Check
+						satoshi: match.object,
+						vendorFieldLength: 255,
+					}),
+					match({
+						activeValidators: 51,
+						address: match.object,
+						block: match.object,
+						blockTime: 9,
+						epoch: match.string,
+						height: 23_000,
+						multiPaymentLimit: 256,
+						reward: "200000000",
+						satoshi: match.object,
+						vendorFieldLength: 255,
+					}),
+				],
+				network: {
+					client: { explorer: "myex.io", symbol: "my", token: "myn" },
+					messagePrefix: "testnet message:\n",
+					name: "testnet",
+					nethash: match.string,
+					pubKeyHash: 168,
+					slip44: 1,
+					wif: 27,
+				},
+			}),
+			{ spaces: 4 },
+		);
+	});
+
+	it("should throw if the core configuration destination already exists", async (context) => {
+		stub(fs, "existsSync").returnValueOnce(true);
+
+		await assert.rejects(
+			() =>
+				context.networkGenerator.generateNetwork({
+					blockTime: 9,
+					coreDBHost: "localhost",
+					coreDBPort: 5432,
+					coreMonitorPort: 4005,
+					coreP2PPort: 4000,
+					coreWebhooksPort: 4004,
+					distribute: true,
+					epoch: new Date(new Date().toISOString().slice(0, 11) + "00:00:00.000Z"),
+					explorer: "myex.io",
+					force: false,
+					maxBlockPayload: 123_444,
+					maxTxPerBlock: 122,
+					network: "testnet",
+					overwriteConfig: false,
+					peers: "127.0.0.1",
+					premine: "12500000000000000",
+					pubKeyHash: 168,
+					rewardAmount: "200000000",
+					rewardHeight: 23_000,
+					symbol: "my",
+					token: "myn",
+					validators: 51,
+					vendorFieldLength: 255,
+					wif: 27,
+				}),
+			`${configCore} already exists.`,
+		);
+	});
+
+	it("should generate a new configuration with additional flags", async (context) => {
+		const existsSync = stub(fs, "existsSync");
+		const ensureDirSync = stub(fs, "ensureDirSync");
+		const writeJSONSync = stub(fs, "writeJSONSync");
+		const writeFileSync = stub(fs, "writeFileSync");
+
+		await context.networkGenerator.generateNetwork({
+			blockTime: 9,
+			coreDBHost: "localhost",
+			coreDBPort: 5432,
+			coreMonitorPort: 4005,
+			coreP2PPort: 4000,
+			coreWebhooksPort: 4004,
+			distribute: true,
+			epoch: new Date(new Date().toISOString().slice(0, 11) + "00:00:00.000Z"),
+			explorer: "myex.io",
+			force: false,
+			maxBlockPayload: 123_444,
+			maxTxPerBlock: 122,
+			network: "testnet",
+			coreDBUsername: "username",
+			overwriteConfig: false,
+			coreDBDatabase: "database",
+			peers: "127.0.0.1",
+			coreDBPassword: "password",
+			premine: "12500000000000000",
+			pubKeyHash: 168,
+			rewardAmount: "200000000",
+			rewardHeight: 23_000,
+			symbol: "my",
+			token: "myn",
+			validators: 51,
+			vendorFieldLength: 255,
+			wif: 27,
+		});
+
+		existsSync.calledWith(configCore);
+		ensureDirSync.calledWith(configCore);
 		writeJSONSync.calledTimes(5);
 		writeFileSync.calledOnce();
 
