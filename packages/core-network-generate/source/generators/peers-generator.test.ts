68999dd6d48be3e85e084aa9f1c849308b73979e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-04 13:43:37 +0000

    Peers

---
+++ packages/core-network-generate/source/generators/peers-generator.test.ts
@@ -0,0 +1,36 @@
+import { existsSync, readJSONSync } from "fs-extra";
+import { join } from "path";
+import { dirSync, setGracefulCleanup } from "tmp";
+
+import { describe } from "../../../core-test-framework";
+import { PeersGenerator } from "./peers-generator";
+
+describe<{
+	dataPath: string;
+	generator: PeersGenerator;
+}>("App generator", ({ it, assert, beforeEach, beforeAll }) => {
+	beforeAll(() => {
+		setGracefulCleanup();
+	});
+
+	beforeEach((context) => {
+		context.dataPath = dirSync().name;
+		context.generator = new PeersGenerator();
+	});
+
+	it("#get - should return data", ({ generator }) => {
+		assert.equal(generator.get(), ["127.0.0.1"]);
+	});
+
+	it("#generate - should set peers", ({ generator }) => {
+		assert.equal(generator.generate(["0.0.0.0"]).get(), ["0.0.0.0"]);
+	});
+
+	it("#write - should write generated data", ({ generator, dataPath }) => {
+		assert.false(existsSync(join(dataPath, "peers.json")));
+
+		generator.write(dataPath);
+
+		assert.equal(readJSONSync(join(dataPath, "peers.json")), { list: ["127.0.0.1"] });
+	});
+});
