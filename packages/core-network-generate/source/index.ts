b8e7ea3dea41fba31c80c545f504cf2a299be8d9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-06 11:40:10 +0000

    Root exports

--- packages/core-network-generate/source/index.ts
+++ packages/core-network-generate/source/index.ts
@@ -1,4 +1,6 @@
 export * from "./application-factory";
 export * from "./configuration-generator";
+export * as Contracts from "./contracts";
 export * as Generators from "./generators";
+export * from "./identifiers";
 export * from "./network-writer";
