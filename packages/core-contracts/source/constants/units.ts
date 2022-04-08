29628defbed65c8307d0220461b42ce6072733ac
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-08 15:13:17 +0000

    Use only decimal

--- packages/core-contracts/source/constants/units.ts
+++ packages/core-contracts/source/constants/units.ts
@@ -1,16 +1,8 @@
 /* eslint sort-keys-fix/sort-keys-fix: 0 */
-export const Decimal = {
-	B: 1,
-	kB: 1000,
-	MB: 1000 ** 2,
-	GB: 1000 ** 3,
-	TB: 1000 ** 4,
-};
-
-export const Binary = {
-	B: 1,
-	KiB: 1024,
-	MiB: 1024 ** 2,
-	GiB: 1024 ** 3,
-	TiB: 1024 ** 4,
+export const Units = {
+	BYTE: 1,
+	KILOBYTE: 1024,
+	MEGABYTE: 1024 ** 2,
+	GIGABYTE: 1024 ** 3,
+	TERABYTE: 1024 ** 4,
 };
