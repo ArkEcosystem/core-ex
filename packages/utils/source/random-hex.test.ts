39b2d786bf16aefb5b23070f1316f10ac29f9c43
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-12 17:08:57 +0000

    random-hex

---
+++ packages/utils/source/random-hex.test.ts
@@ -0,0 +1,11 @@
+import { describe } from "@arkecosystem/core-test-framework";
+
+import { randomHex } from "./random-hex";
+
+describe("#randomHex", ({ it, assert }) => {
+	it("should return a random hex string", () => {
+		assert.length(randomHex(8), 8);
+		assert.length(randomHex(16), 16);
+		assert.length(randomHex(32), 32);
+	});
+});
