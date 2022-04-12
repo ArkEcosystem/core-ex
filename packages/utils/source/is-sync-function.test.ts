1e0f92a132090b72899cce134d2290fd1d0bfe82
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-12 16:51:51 +0000

    is-sync-function.test

---
+++ packages/utils/source/is-sync-function.test.ts
@@ -0,0 +1,14 @@
+import { describe } from "@arkecosystem/core-test-framework";
+
+import { isSyncFunction } from "./is-sync-function";
+
+describe("#isSyncFunction", ({ it, assert }) => {
+	it("should pass", () => {
+		assert.true(isSyncFunction(new Function()));
+	});
+
+	it("should fail", () => {
+		assert.false(isSyncFunction(async () => ({})));
+		assert.false(isSyncFunction([]));
+	});
+});
