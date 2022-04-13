07df93ef874242a3fa6194f4c8ace37a7fd236fd
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-13 13:30:25 +0000

    Fix env-paths

--- packages/core-cli/source/env-paths.ts
+++ packages/core-cli/source/env-paths.ts
@@ -1,6 +1,8 @@
-import { default } from "env-paths";
+/* eslint-disable import/no-namespace */
+/* eslint-disable unicorn/prevent-abbreviations */
+import * as envPathsImport from "env-paths";
 export { Paths } from "env-paths";
 
 export const envPaths = {
-	get: default,
+	get: envPathsImport.default,
 };
