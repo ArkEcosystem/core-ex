8a49ab4889dd7bb82a6b0a0ab5efe8e730c9bda9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-19 11:16:05 +0000

    Serialize

---
+++ packages/core-crypto-block/test/fixtures/block.ts
@@ -0,0 +1,19 @@
+import { BigNumber } from "@arkecosystem/utils";
+
+export const blockData = {
+	blockSignature:
+		"46b36954f4d5b5fdfa81af97f4fb7aafc160e820e7f35c3c42bcae027dedf7a8e371051c55845c78b3dec8d9e54855a315f7047ce00b810681c90c6457c5d324",
+	generatorPublicKey: "463db0108677602474d773785804bde662a4e887f9b71d51fe04904c08a654e3",
+	height: 2,
+	id: "f293605a39d3759d5711dfd03a979b33ef8d6c0b5454f100bd8692e4a936866b",
+	numberOfTransactions: 0,
+	payloadHash: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+	payloadLength: 0,
+	previousBlock: "0000000000000000000000000000000000000000000000000000000000000000",
+	reward: BigNumber.ZERO,
+	timestamp: 0,
+	totalAmount: BigNumber.ZERO,
+	totalFee: BigNumber.ZERO,
+	transactions: [],
+	version: 1,
+};
