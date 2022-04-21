3d32779cb919d6de7e864e7d3aa412f15b6f5a11
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-04-21 09:19:16 +0000

    prepare-sandbox

--- packages/core-crypto-block/source/factory.test.ts
+++ packages/core-crypto-block/source/factory.test.ts
@@ -1,23 +1,8 @@
-import { Contracts, Identifiers } from "@arkecosystem/core-contracts";
+import { Contracts } from "@arkecosystem/core-contracts";
 import clone from "lodash.clone";
 
 import crypto from "../../core/bin/config/testnet/crypto.json";
-import { ServiceProvider as CoreCryptoAddressBech32m } from "../../core-crypto-address-bech32m";
-import { ServiceProvider as CoreCryptoConfig } from "../../core-crypto-config";
-import { Configuration } from "../../core-crypto-config/source/configuration";
-import { ServiceProvider as CoreCryptoHashBcrypto } from "../../core-crypto-hash-bcrypto";
-import { ServiceProvider as CoreCryptoKeyPairSchnorr } from "../../core-crypto-key-pair-schnorr";
-import { ServiceProvider as CoreCryptoSignatureSchnorr } from "../../core-crypto-signature-schnorr";
-import { ServiceProvider as CoreCryptoTime } from "../../core-crypto-time";
-import { ServiceProvider as CoreCryptoTransaction } from "../../core-crypto-transaction";
-import { ServiceProvider as CoreCryptoTransactionTransfer } from "../../core-crypto-transaction-transfer";
-import { ServiceProvider as CoreCryptoValidation } from "../../core-crypto-validation";
-import { ServiceProvider as CoreCryptoWif } from "../../core-crypto-wif";
-import { ServiceProvider as CoreFees } from "../../core-fees";
-import { ServiceProvider as CoreFeesStatic } from "../../core-fees-static";
-import { ServiceProvider as CoreSerializer } from "../../core-serializer";
 import { describe, Factories, Sandbox } from "../../core-test-framework";
-import { ServiceProvider as CoreValidation } from "../../core-validation";
 import {
 	blockData,
 	blockDataJson,
@@ -27,9 +12,8 @@ import {
 	serializedWithTransactions,
 } from "../test/fixtures/block";
 import { assertBlockData, assertTransactionData } from "../test/helpers/asserts";
-import { Deserializer } from "./deserializer";
+import { prepareSandbox } from "../test/helpers/prepare-sandbox";
 import { BlockFactory } from "./factory";
-import { IDFactory } from "./id.factory";
 import { Serializer } from "./serializer";
 interface Identity {
 	keys: Contracts.Crypto.IKeyPair;
@@ -57,27 +41,7 @@ describe<{
 		blockDataClone = clone(blockDataOriginal);
 		blockDataWithTransactionsClone = clone(blockDataWithTransactionsOriginal);
 
-		context.sandbox = new Sandbox();
-
-		await context.sandbox.app.resolve(CoreSerializer).register();
-		await context.sandbox.app.resolve(CoreValidation).register();
-		await context.sandbox.app.resolve(CoreCryptoConfig).register();
-		await context.sandbox.app.resolve(CoreCryptoTime).register();
-		await context.sandbox.app.resolve(CoreCryptoValidation).register();
-		await context.sandbox.app.resolve(CoreCryptoHashBcrypto).register();
-		await context.sandbox.app.resolve(CoreCryptoSignatureSchnorr).register();
-		await context.sandbox.app.resolve(CoreCryptoKeyPairSchnorr).register();
-		await context.sandbox.app.resolve(CoreCryptoAddressBech32m).register();
-		await context.sandbox.app.resolve(CoreCryptoWif).register();
-		await context.sandbox.app.resolve(CoreFees).register();
-		await context.sandbox.app.resolve(CoreFeesStatic).register();
-		await context.sandbox.app.resolve(CoreCryptoTransaction).register();
-		await context.sandbox.app.resolve(CoreCryptoTransactionTransfer).register();
-		context.sandbox.app.bind(Identifiers.Cryptography.Block.Serializer).to(Serializer);
-		context.sandbox.app.bind(Identifiers.Cryptography.Block.Deserializer).to(Deserializer);
-		context.sandbox.app.bind(Identifiers.Cryptography.Block.IDFactory).to(IDFactory);
-
-		context.sandbox.app.get<Configuration>(Identifiers.Cryptography.Configuration).setConfig(crypto);
+		await prepareSandbox(context);
 
 		context.factory = context.sandbox.app.resolve(BlockFactory);
 		context.serializer = context.sandbox.app.resolve(Serializer);
--- packages/core-crypto-block/source/id.factory.test.ts
+++ packages/core-crypto-block/source/id.factory.test.ts
@@ -1,51 +1,14 @@
-import { Identifiers } from "@arkecosystem/core-contracts";
-
-import crypto from "../../core/bin/config/testnet/crypto.json";
-import { ServiceProvider as CoreCryptoAddressBech32m } from "../../core-crypto-address-bech32m";
-import { ServiceProvider as CoreCryptoConfig } from "../../core-crypto-config";
-import { Configuration } from "../../core-crypto-config/source/configuration";
-import { ServiceProvider as CoreCryptoHashBcrypto } from "../../core-crypto-hash-bcrypto";
-import { ServiceProvider as CoreCryptoKeyPairSchnorr } from "../../core-crypto-key-pair-schnorr";
-import { ServiceProvider as CoreCryptoSignatureSchnorr } from "../../core-crypto-signature-schnorr";
-import { ServiceProvider as CoreCryptoTime } from "../../core-crypto-time";
-import { ServiceProvider as CoreCryptoTransaction } from "../../core-crypto-transaction";
-import { ServiceProvider as CoreCryptoTransactionTransfer } from "../../core-crypto-transaction-transfer";
-import { ServiceProvider as CoreCryptoValidation } from "../../core-crypto-validation";
-import { ServiceProvider as CoreCryptoWif } from "../../core-crypto-wif";
-import { ServiceProvider as CoreFees } from "../../core-fees";
-import { ServiceProvider as CoreFeesStatic } from "../../core-fees-static";
-import { ServiceProvider as CoreSerializer } from "../../core-serializer";
 import { describe, Sandbox } from "../../core-test-framework";
-import { ServiceProvider as CoreValidation } from "../../core-validation";
 import { blockData } from "../test/fixtures/block";
+import { prepareSandbox } from "../test/helpers/prepare-sandbox";
 import { IDFactory } from "./id.factory";
-import { Serializer } from "./serializer";
 
 describe<{
 	sandbox: Sandbox;
 	idFactory: IDFactory;
 }>("IdFactory", ({ it, assert, beforeEach }) => {
 	beforeEach(async (context) => {
-		context.sandbox = new Sandbox();
-
-		await context.sandbox.app.resolve(CoreSerializer).register();
-		await context.sandbox.app.resolve(CoreValidation).register();
-		await context.sandbox.app.resolve(CoreCryptoConfig).register();
-		await context.sandbox.app.resolve(CoreCryptoTime).register();
-		await context.sandbox.app.resolve(CoreCryptoValidation).register();
-		await context.sandbox.app.resolve(CoreCryptoHashBcrypto).register();
-		await context.sandbox.app.resolve(CoreCryptoSignatureSchnorr).register();
-		await context.sandbox.app.resolve(CoreCryptoKeyPairSchnorr).register();
-		await context.sandbox.app.resolve(CoreCryptoAddressBech32m).register();
-		await context.sandbox.app.resolve(CoreCryptoWif).register();
-		await context.sandbox.app.resolve(CoreFees).register();
-		await context.sandbox.app.resolve(CoreFeesStatic).register();
-		await context.sandbox.app.resolve(CoreCryptoTransaction).register();
-		await context.sandbox.app.resolve(CoreCryptoTransactionTransfer).register();
-		context.sandbox.app.bind(Identifiers.Cryptography.Block.Serializer).to(Serializer);
-		context.sandbox.app.bind(Identifiers.Cryptography.Block.IDFactory).to(IDFactory);
-
-		context.sandbox.app.get<Configuration>(Identifiers.Cryptography.Configuration).setConfig(crypto);
+		await prepareSandbox(context);
 
 		context.idFactory = context.sandbox.app.resolve(IDFactory);
 	});
