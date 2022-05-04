664161ce266704f6408e23b0ca41464f964321ea
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-05-04 13:24:45 +0000

    Move validPeer format

--- packages/core-crypto-validation/source/service-provider.ts
+++ packages/core-crypto-validation/source/service-provider.ts
@@ -1,25 +1,14 @@
 import { Contracts, Identifiers } from "@arkecosystem/core-contracts";
 import { Providers } from "@arkecosystem/core-kernel";
 
-import { registerFormats } from "./formats";
 import { registerKeywords } from "./keywords";
 import { schemas } from "./schemas";
 
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
-		await this.#registerFormats();
-
 		await this.#registerKeywords();
 
 		await this.#registerSchemas();
-	}
-
-	async #registerFormats(): Promise<void> {
-		for (const format of Object.values(registerFormats(this.app.get(Identifiers.Cryptography.Configuration)))) {
-			this.app.get<Contracts.Crypto.IValidator>(Identifiers.Cryptography.Validator).extend((ajv) => {
-				format(ajv);
-			});
-		}
 	}
 
 	async #registerKeywords(): Promise<void> {
