1c4a0e7d9f777afb19e91da5c7d4ce6853796145
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-05-06 09:34:16 +0000

    Improve address schema

--- packages/core-crypto-address-bech32m/source/schemas.ts
+++ packages/core-crypto-address-bech32m/source/schemas.ts
@@ -3,9 +3,9 @@ export const schemas = {
 		$id: "address",
 		allOf: [
 			{
+				$ref: "alphanumeric",
 				maxLength: 62,
 				minLength: 62,
-				pattern: "^[0123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]+$",
 			},
 		],
 		type: "string",
